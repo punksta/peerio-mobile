aa8f135c2ec3d6ec49ea682cd9ec540cd5d47b8d
Samvel Avanesov <seavan@gmail.com>
2016-08-19 16:20:51 +0300

    chore(project): easing up on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": [
         "peerio"
     ],
-    "parser": "babel-eslint"
+    "parser": "babel-eslint",
+    "rules": {
+        "no-unused-vars": 0,
+        "react/prefer-stateless-function": 0
+    }
 };
