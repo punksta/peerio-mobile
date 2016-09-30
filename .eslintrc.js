ed4a639d41c698f0984f8a002e38d59ec8f772bf
Samvel Avanesov <seavan@gmail.com>
2016-09-30 14:55:22 +0300

    chore(project): remove eslint react sorting rule (ci-skip)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         'global-require': 0,
         'react/prefer-stateless-function': 0,
         'react/jsx-filename-extension': 0,
+        'react/sort-comp': 0,
         'no-mixed-operators': 0
     },
     globals: {
