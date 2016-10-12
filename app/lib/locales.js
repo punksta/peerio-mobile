16994f3fbbb4483fc7ba120eb9ee513351c600f5
Samvel Avanesov <seavan@gmail.com>
2016-10-12 14:08:26 +0300

    feat(locale): add passphrase generator locales
    task #854

---
+++ app/lib/locales.js
@@ -0,0 +1,14 @@
+import RNFS from 'react-native-fs';
+
+module.exports = {
+    loadLocaleFile(lc) {
+        const def = require('peerio-copy/client_en.json');
+        const path = `${RNFS.MainBundlePath}/locales/${lc}.json`;
+        return RNFS.exists(path)
+            .then(exists => (exists ? RNFS.readFile(path).then(JSON.parse) : def))
+            .catch(e => {
+                console.error(e);
+                return def;
+            });
+    }
+};
