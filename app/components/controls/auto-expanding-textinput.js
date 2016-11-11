7f2f90ea75f64d9570a3ac1f13f42b0c556a1090
Samvel Avanesov <seavan@gmail.com>
2016-11-11 14:55:52 +0300

    feat(ui): fork auto expanding textinput

---
+++ app/components/controls/auto-expanding-textinput.js
@@ -0,0 +1,74 @@
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+const styles = StyleSheet.create({
+    default: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        justifyContent: 'center',
+        textAlign: 'left'
+    }
+});
+
+export default class AutoExpandingTextInput extends Component {
+
+    constructor(props) {
+        super(props);
+        // initial state
+        this.state = {
+            height: this.props.minHeight,
+            maxHeight: this.props.maxHeight || this.props.minHeight * 3
+        };
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    static propTypes = {
+        onChangeHeight: PropTypes.func.isRequired,
+        minHeight: PropTypes.number.isRequired,
+        maxHeight: PropTypes.number
+    };
+
+    static defaultProps = {};
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value === '') {
+            this.setState({
+                height: this.props.minHeight
+            });
+        }
+    }
+
+    _onChange = (event) => {
+        const curHeight = event.nativeEvent.contentSize.height;
+        if (curHeight < this.props.minHeight || curHeight > this.state.maxHeight) return;
+
+        if (this.state.height !== curHeight) {
+            if (this.props.onChangeHeight) {
+                this.props.onChangeHeight(this.state.height, curHeight);
+            }
+        }
+
+        this.setState({
+            height: curHeight
+        });
+    }
+
+    render() {
+        const tmpHeight = Math.min(this.state.maxHeight, this.state.height);
+        return (
+            <TextInput
+                {...this.props}
+                ref={ti => (this.ti = ti)}
+                multiline
+                onChange={this._onChange}
+                style={[styles.default, this.props.style, { height: tmpHeight }]}
+            />
+        );
+    }
+}
+
+AutoExpandingTextInput.propTypes = {
+    style: React.PropTypes.any
+};
