765b66c92e67f489fd793cc709b356581fe0d139
Samvel Avanesov <seavan@gmail.com>
2016-11-11 15:21:00 +0300

    chore(project): copy peerio-icebear to app/lib automatically

--- setup-env.sh
+++ setup-env.sh
@@ -2,5 +2,5 @@ if [ ! -f env.sh ]; then
   echo "export PEERIO_SOCKET_SERVER=$PEERIO_SOCKET_SERVER" > env.sh
 fi
 if [ ! -d app/lib/peerio-icebear ]; then
-  ln -s ../../../peerio-icebear/src app/lib/peerio-icebear
+  cp -r node_modules/peerio-icebear app/lib/peerio-icebear
 fi
