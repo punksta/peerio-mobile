c467652efb02cede393d262d81933868c21d9317
Samvel Avanesov <seavan@gmail.com>
2016-11-12 18:55:11 +0300

    chore(travis): more tests

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,7 @@
 echo "Checking env.sh"
 echo `pwd`
 ls -la app/lib
+ls -la app/lib/peerio-icebear
 if [ ! -f env.sh ]; then
   echo "Creating env.sh"
   echo "export PEERIO_SOCKET_SERVER=$PEERIO_SOCKET_SERVER" > env.sh
