e2d6fd5f28dbaa6fa8e7eebd9070d922fb7bda2a
Samvel Avanesov <seavan@gmail.com>
2016-11-12 18:34:34 +0300

    chore(travis): build logs

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,11 @@
+echo "Checking env.sh"
 if [ ! -f env.sh ]; then
+  echo "Creating env.sh"
   echo "export PEERIO_SOCKET_SERVER=$PEERIO_SOCKET_SERVER" > env.sh
   source env.sh
 fi
+echo "Checking icebear lib existence"
 if [ ! -d app/lib/peerio-icebear ]; then
+  echo "Copying icebear lib"
   cp -r node_modules/peerio-icebear app/lib/peerio-icebear
 fi
