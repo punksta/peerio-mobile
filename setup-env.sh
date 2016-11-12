36cefbe841919d91dfc84a32ea7a64ae73aec20d
Samvel Avanesov <seavan@gmail.com>
2016-11-12 17:54:38 +0300

    chore(project): automatically source env on install

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,6 @@
 if [ ! -f env.sh ]; then
   echo "export PEERIO_SOCKET_SERVER=$PEERIO_SOCKET_SERVER" > env.sh
+  source env.sh
 fi
 if [ ! -d app/lib/peerio-icebear ]; then
   cp -r node_modules/peerio-icebear app/lib/peerio-icebear
