25d1fe4003c60e800238a9c0e41a4af9514015e1
Samvel Avanesov <seavan@gmail.com>
2016-11-18 18:09:29 +0300

    chore(ui): refactor placeholder to contain snackbar

---
+++ app/components/layout/placeholder.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+    View, ActivityIndicator
+} from 'react-native';
+import SnackBar from '../snackbars/snackbar';
+import mainState from '../main/main-state';
+
+export default class Placeholder extends Component {
+    render() {
+        const s = {
+            flex: 1,
+            justifyContent: 'space-between'
+        };
+        const ind = mainState.loading ?
+            <ActivityIndicator style={{ paddingTop: 10 }} /> : <View />;
+        const sb = mainState.loading ? null : <SnackBar />;
+        return (
+            <View style={s}>
+                {ind}
+                {sb}
+            </View>
+        );
+    }
+}
+
