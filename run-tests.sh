f88229ad278f0e3866df037cb2cd8dfcd429e3c9
Samvel Avanesov <seavan@gmail.com>
2016-12-30 02:48:17 +0300

    WIP: tests

--- run-tests.sh
+++ run-tests.sh
@@ -4,8 +4,9 @@ source .pyenv/bin/activate
 # prepare circleci
 sim=/Applications/Xcode-8.0.app/Contents/Developer/Applications/Simulator.app
 device=33D34EBA-703E-4A82-8838-BE75171492E1
-if [ -e $sim ]; then
+if [ -d $sim ]; then
   # prelaunch sim so there's no timeout
+  echo "launching sim"
   open -Fn $sim --args -CurrentDeviceUDID $device
 fi
 
