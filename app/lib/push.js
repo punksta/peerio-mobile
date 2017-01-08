ae34f76d6109e4bf2e93621b7a13c97d72f0a75e
Samvel Avanesov <seavan@gmail.com>
2017-01-08 20:23:24 +0300

    feat(android): add push notification support
    task #2170

--- app/lib/push.js
+++ app/lib/push.js
@@ -1,19 +1,25 @@
 import PushNotification from 'react-native-push-notification';
+import { Platform } from 'react-native';
 import { socket } from '../lib/icebear';
+import { when } from 'mobx';
 
 function enablePushNotifications() {
     PushNotification.configure({
         onRegister(token) {
-            console.log('push.js: TOKEN:', token);
-            socket.send('registerMobileDevice', { ios: token })
-                .then(r => console.log('push.js: register result success', r))
-                .catch(e => console.error('push.js: error registering', e));
+            console.log(`push.js: OS: ${Platform.OS}, TOKEN: ${token.token}`);
+            const payload = {};
+            payload[Platform.OS] = token.token || token;
+            when(() => socket.authenticated, () =>
+                 socket.send('/auth/registerMobileDevice', payload)
+                     .then(r => console.log('push.js: register result success', r))
+                     .catch(e => console.error('push.js: error registering', e)));
         },
 
         onNotification(notification) {
             console.log('push.js: NOTIFICATION:', notification);
         },
 
+        // GCM sender id
         senderID: '605156423279',
 
         permissions: {
