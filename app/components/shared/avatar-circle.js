6591624f45f2a651387db6b8fef7c0d2e5f431da
Samvel Avanesov <seavan@gmail.com>
2017-02-15 02:34:27 +0300

    feat(ui): styling for contact tofu error

--- app/components/shared/avatar-circle.js
+++ app/components/shared/avatar-circle.js
@@ -22,22 +22,41 @@ export default class AvatarCircle extends Component {
             margin: 4,
             marginTop: 10
         };
+        const tofuStyle = {
+            position: 'absolute',
+            right: 0,
+            width: width / 2,
+            height: height / 2,
+            borderRadius: width / 4,
+            borderColor: 'white',
+            borderWidth: 1,
+            backgroundColor: 'red',
+            overflow: 'hidden',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: 4,
+            marginTop: 10
+        };
 
         if (this.props.loading) {
             return <ActivityIndicator style={{ height, margin: 4 }} />;
         }
         const { username, firstName, color, tofuError } = this.props.contact;
-        const bgColor = tofuError ? 'red' : color;
-        const letter = tofuError ? '!' : (firstName || username || ' ')[0].toUpperCase();
+        const letter = (firstName || username || ' ')[0].toUpperCase();
         const coloredAvatarStyle = [avatarStyle, {
             overflow: 'hidden',
             justifyContent: 'center',
             alignItems: 'center',
-            backgroundColor: bgColor || '#fff'
+            backgroundColor: color || '#fff'
         }];
         return (
-            <View style={coloredAvatarStyle}>
-                <Text style={{ color: 'white', fontSize: 12 * ratio }}>{letter}</Text>
+            <View>
+                <View style={coloredAvatarStyle}>
+                    <Text style={{ color: 'white', fontSize: 12 * ratio }}>{letter}</Text>
+                </View>
+                <View style={tofuStyle}>
+                    <Text style={{ color: 'white', fontSize: 10 * ratio, fontWeight: 'bold' }}>!</Text>
+                </View>
             </View>
         );
     }
