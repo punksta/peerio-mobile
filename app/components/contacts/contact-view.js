3fb5c5904c84899a98ae8ead2bfa28e6a45b06c2
Samvel Avanesov <seavan@gmail.com>
2017-03-01 16:24:12 +0300

    feat(ui): beautify contact view
    task #2691

--- app/components/contacts/contact-view.js
+++ app/components/contacts/contact-view.js
@@ -7,6 +7,7 @@ import { observer } from 'mobx-react/native';
 import AvatarCircle from '../shared/avatar-circle';
 import LayoutModalExit from '../layout/layout-modal-exit';
 import mainState from '../main/main-state';
+import { vars } from '../../styles/styles';
 
 const flexRow = {
     flexDirection: 'row',
@@ -22,25 +23,28 @@ export default class ContactView extends Component {
         const contact = this.props.contact || mainState.currentContact;
         const { username, firstName, lastName, fingerprint, tofuError } = contact;
         const tofuErrorControl = tofuError && (
-            <View style={{ backgroundColor: 'red', flexGrow: 1, padding: 10 }}>
-                <Text>
-                    This contact{'\''}s public key has CHANGED, which means it may be compromised.
-                    Hit the gym, lawyer up, delete the Facebook.
+            <View style={{ backgroundColor: '#D0021B', flexGrow: 1, padding: 10, marginVertical: 24 }}>
+                <Text style={{ color: vars.white }}>
+                    This contact{'\''}s public key has changed, which means it may be compromised.
                 </Text>
             </View>
         );
+        let i = 0;
+        const fingerprintFormatted = fingerprint.replace(/-/g, () => (i++ === 2 ? '\n' : ' '));
         const body = (
             <View style={{ padding: 8 }}>
                 <View style={flexRow}>
                     <AvatarCircle contact={contact} large />
                     <View>
-                        <Text>Name: {firstName} {lastName}</Text>
-                        <Text>Username: @{username}</Text>
+                        <Text style={{ fontWeight: 'bold', color: vars.txtDark, fontSize: 16, marginVertical: 10 }}>{firstName} {lastName}</Text>
+                        <Text style={{ color: vars.txtMedium }}>@{username}</Text>
                     </View>
                 </View>
                 {tofuErrorControl}
-                <Text>Fingerprint:</Text>
-                <Text>{fingerprint}</Text>
+                <Text style={{ color: vars.txtDate, marginVertical: 10 }}>Fingerprint:</Text>
+                <Text style={{ color: vars.txtMedium }} numberOfLines={2}>
+                    {fingerprintFormatted}
+                </Text>
             </View>
         );
         return <LayoutModalExit body={body} title={username} />;
