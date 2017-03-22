9c425089eba07f341459f30aa78e39e734b1ca28
Samvel Avanesov <seavan@gmail.com>
2017-03-22 17:37:59 +0300

    feat(android): in-app-billing and key

--- android/app/src/main/java/com/peerio/MainApplication.java
+++ android/app/src/main/java/com/peerio/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.idehub.Billing.InAppBillingBridgePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import cl.json.RNSharePackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+              new InAppBillingBridgePackage(),
           new VectorIconsPackage(),
           new RNSoundPackage(),
           new ReactNativeRestartPackage(),
