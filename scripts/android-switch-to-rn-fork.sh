0c3f30162a9dd19e1aa4420841a7a7d5e60c8f53
Samvel Avanesov <seavan@gmail.com>
2017-04-07 19:11:06 +0300

    fix(build): set android patching to 4 levels depth

--- scripts/android-switch-to-rn-fork.sh
+++ scripts/android-switch-to-rn-fork.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find node_modules -name "build.gradle" -maxdepth 3 | xargs sed -i.bak -e "s/^.*compile .*com.facebook.react:react-native:.*$/    compile project(':ReactAndroid')/"
+find node_modules -name "build.gradle" -maxdepth 4 | xargs sed -i.bak -e "s/^.*compile .*com.facebook.react:react-native:.*$/    compile project(':ReactAndroid')/"
