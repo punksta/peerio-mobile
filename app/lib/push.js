07993cdc1883f4f37a25a4446d360ff0992335d4
Samvel Avanesov <seavan@gmail.com>
2017-04-11 04:12:27 +0300

    fix(pushes): contract update

--- app/lib/push.js
+++ app/lib/push.js
@@ -3,27 +3,33 @@ import { Platform } from 'react-native';
 import { when } from 'mobx';
 import { socket } from '../lib/icebear';
 
+const TEST_TOKEN = 'a41f1b2e8e9279c81dd9c69c56fd060d25c743354a146d4d9dcddcd9bf73b0e6';
+
+function onRegister(token) {
+    console.log(`push.js: OS: ${Platform.OS}, TOKEN: ${token.token}`);
+    const payload = {};
+    payload[Platform.OS] = token.token || token;
+    when(() => socket.authenticated, () => {
+        console.log(`push.js: OS: ${JSON.stringify(payload)}`);
+        socket.send('/auth/mobile-device/register', payload)
+            .then(r => {
+                console.log(`push.js: register result success ${JSON.stringify(r)}`);
+                // PushNotification.localNotification({ title: 'test', message: 'testmessage' });
+            })
+            // .then(() => {
+            //     return socket.send('/auth/dev/test-push', {
+            //         icon: 'push',
+            //         text: 'blah'
+            //     });
+            // })
+            // .then(console.log.bind(console))
+            .catch(e => console.log('push.js: error registering', e));
+    });
+}
+
 function enablePushNotifications() {
     PushNotification.configure({
-        onRegister(token) {
-            console.log(`push.js: OS: ${Platform.OS}, TOKEN: ${token.token}`);
-            const payload = {};
-            payload[Platform.OS] = token.token || token;
-            when(() => socket.authenticated, () =>
-                 socket.send('/auth/registerMobileDevice', payload)
-                     .then(r => {
-                         console.log('push.js: register result success', r);
-                         // PushNotification.localNotification({ title: 'test', message: 'testmessage' });
-                     })
-                     // .then(() => {
-                     //     return socket.send('/auth/dev/test-push', {
-                     //         icon: 'push',
-                     //         text: 'blah'
-                     //     });
-                     // })
-                     // .then(console.log.bind(console))
-                     .catch(e => console.log('push.js: error registering', e)));
-        },
+        onRegister,
 
         onNotification(notification) {
             console.log('push.js: NOTIFICATION:', notification);
@@ -58,5 +64,9 @@ function disableServerSide() {
              .catch(e => console.error(e))
         );
 }
+
+// if (__DEV__) {
+//     onRegister({ token: TEST_TOKEN });
+// }
 
 module.exports = { enablePushNotifications, enableServerSide, disableServerSide };
