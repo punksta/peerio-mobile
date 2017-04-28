49ec8de1ad1e33a249d9bd7e9c84d2d64885ddd8
Samvel Avanesov <seavan@gmail.com>
2017-04-28 14:09:15 +0300

    feat(crypto): scrypt-native js

---
+++ app/lib/scrypt-native.js
@@ -0,0 +1,17 @@
+import { View, Text, PanResponder, AppState, ActivityIndicator, NativeModules } from 'react-native';
+import { b64ToBytes, bytesToB64 } from './worker';
+
+function scryptNative(password, salt, options, callback) {
+    const passwordB64 = bytesToB64(password);
+    const saltB64 = bytesToB64(salt);
+    options.p = options.p || 1;
+    const { N, r, p, dkLen } = options;
+    const start = (new Date()).getTime();
+    return NativeModules.RNSodium.scrypt(passwordB64, saltB64, N, r, p, dkLen)
+        .then(result => callback(b64ToBytes(result)))
+        .then(() => console.log(`scrypt-worker.js: executed in ${(new Date()).getTime() - start}`));
+}
+
+module.exports = {
+    scryptNative
+};
