252b4cc55ada7927b812eb04f22f9355943901aa
Samvel Avanesov <seavan@gmail.com>
2017-04-28 14:08:41 +0300

    feat(crypto): temporary adding sodium lib to ios project

---
+++ ios/peeriomobile/RNSodium.h
@@ -0,0 +1,14 @@
+//
+//  RNTouchIDValue.h
+//  RNTouchIDValue
+//
+//  Created by Samvel Avanesov on 05/01/2017.
+//  Copyright © 2017 Technologies Peerio Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RNTouchIDValue : NSObject <RCTBridgeModule>
+
+@end
