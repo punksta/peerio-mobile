cb1081291d5520543d6b28d279a2b9a5f9928849
Samvel Avanesov <seavan@gmail.com>
2017-05-07 01:41:25 +0300

    fix(tests): correct testid for popup ok buttons

--- app/components/shared/popups.js
+++ app/components/shared/popups.js
@@ -31,7 +31,7 @@ const swActions = {
 };
 
 function popupSystemWarning(title, contents, buttons) {
-    const button = (text, action) => ({ id: text, text, action });
+    const button = (text, action) => ({ id: 'ok', text, action });
     const swButton = i => ({ id: i.action, text: t(i.label), action: swActions[i.action] });
     return new Promise((resolve) => {
         popupState.showPopup({
