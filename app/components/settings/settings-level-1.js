c2a56cebcd52bfb4f087fdf809e6f98d54412024
Samvel Avanesov <seavan@gmail.com>
2017-05-09 00:44:32 +0300

    feat(ui): return log out to settings

--- app/components/settings/settings-level-1.js
+++ app/components/settings/settings-level-1.js
@@ -3,19 +3,19 @@ import { View, ScrollView } from 'react-native';
 import { observer } from 'mobx-react/native';
 import { vars } from '../../styles/styles';
 import SettingsItem from './settings-item';
-import settingsState from './settings-state';
-import snackbarState from '../snackbars/snackbar-state';
-import mainState from '../main/main-state';
+import { settingsState, snackbarState, mainState, loginState } from '../states';
 import { PaymentStorageUsage, paymentCheckout } from '../payments/payments-storage-usage';
 import { toggleConnection } from '../main/dev-menu-items';
-import { t } from '../utils/translator';
 
 const bgStyle = {
     flexGrow: 1,
     flex: 1,
-    paddingVertical: vars.listViewPaddingVertical,
-    paddingHorizontal: vars.listViewPaddingHorizontal,
     backgroundColor: vars.settingsBg
+};
+
+const svStyle = {
+    paddingVertical: vars.listViewPaddingVertical,
+    paddingHorizontal: vars.listViewPaddingHorizontal
 };
 
 @observer
@@ -26,22 +26,28 @@ export default class SettingsLevel1 extends Component {
 
     render() {
         return (
-            <ScrollView style={bgStyle}>
-                {/* <SettingsItem title="title_settingsProfile" disabled /> */}
-                <SettingsItem title="title_settingsSecurity" onPress={() => settingsState.transition('security')} />
-                {/* <SettingsItem title="title_settingsPreferences" disabled /> */}
-                <SettingsItem title="title_help" onPress={() => settingsState.routerMain.logs()} />
-                {this.spacer}
-                <SettingsItem title="title_storageUsage" icon={null} onPress={paymentCheckout}>
-                    <PaymentStorageUsage />
-                </SettingsItem>
-                {this.spacer}
-                {__DEV__ && <SettingsItem title="Toggle connection" onPress={toggleConnection} />}
-                {__DEV__ && <SettingsItem title="Damage TouchID" onPress={() => mainState.damageUserTouchId()} />}
-                {__DEV__ && <SettingsItem title="Snackbar" onPress={() => snackbarState.pushTemporary('test')} />}
-                {/* <SettingsItem title={t('payments')} onPress={() => settingsState.transition('payments')} /> */}
-                {/* <SettingsItem title={t('quotas')} onPress={() => settingsState.transition('quotas')} /> */}
-            </ScrollView>
+            <View style={bgStyle}>
+                <ScrollView contentContainerStyle={svStyle}>
+                    {/* <SettingsItem title="title_settingsProfile" disabled /> */}
+                    <SettingsItem title="title_settingsSecurity" onPress={() => settingsState.transition('security')} />
+                    {/* <SettingsItem title="title_settingsPreferences" disabled /> */}
+                    <SettingsItem title="title_help" onPress={() => settingsState.routerMain.logs()} />
+                    {this.spacer}
+                    <SettingsItem title="title_storageUsage" icon={null} onPress={paymentCheckout}>
+                        <PaymentStorageUsage />
+                    </SettingsItem>
+                    {this.spacer}
+                    <SettingsItem title="title_help" onPress={() => settingsState.routerMain.logs()} />
+                    {this.spacer}
+                    <SettingsItem title="button_logout" onPress={() => loginState.signOut()} />
+                    {this.spacer}
+                    {__DEV__ && <SettingsItem title="Toggle connection" onPress={toggleConnection} />}
+                    {__DEV__ && <SettingsItem title="Damage TouchID" onPress={() => mainState.damageUserTouchId()} />}
+                    {__DEV__ && <SettingsItem title="Snackbar" onPress={() => snackbarState.pushTemporary('test')} />}
+                    {/* <SettingsItem title={t('payments')} onPress={() => settingsState.transition('payments')} /> */}
+                    {/* <SettingsItem title={t('quotas')} onPress={() => settingsState.transition('quotas')} /> */}
+                </ScrollView>
+            </View>
         );
     }
 }
---
+++ app/components/states.js
@@ -0,0 +1,6 @@
+import settingsState from './settings/settings-state';
+import snackbarState from './snackbars/snackbar-state';
+import mainState from './main/main-state';
+import loginState from './login/login-state';
+
+export { settingsState, snackbarState, mainState, loginState };
