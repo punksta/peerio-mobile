94a9f2234c095f26580f176aeeef98cf3694afb0
Samvel Avanesov <seavan@gmail.com>
2017-05-10 02:48:27 +0300

    fix(ui): don't show snackbar right after backgrounding

--- app/components/snackbars/snackbar-connection.js
+++ app/components/snackbars/snackbar-connection.js
@@ -1,12 +1,12 @@
 import { observer } from 'mobx-react/native';
 import SnackbarBase from './snackbar-base';
-import snackbarState from './snackbar-state';
+import uiState from '../layout/ui-state';
 import { socket } from '../../lib/icebear';
 import { t } from '../utils/translator';
 
 @observer
 export default class SnackbarConnection extends SnackbarBase {
-    get isVisible() { return !socket.connected; }
+    get isVisible() { return uiState.appState === 'active' && !socket.connected; }
 
     // to override
     getText() {
@@ -15,10 +15,10 @@ export default class SnackbarConnection extends SnackbarBase {
 
     // to override
     getShowDelay() {
-        return 2000;
+        return 5000;
     }
 
     tap() {
-        this.hide(() => snackbarState.pop());
+        this.hide();
     }
 }
