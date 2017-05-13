b4a3a3875bb3d1dc2cce11acd11617bd243edc78
Eren Bilgin <eren@peerio.com>
2017-05-12 21:52:16 -0400

    chore(l18n): sign in and create account

--- app/components/login/login-start.js
+++ app/components/login/login-start.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { observer } from 'mobx-react/native';
+import { tu } from '../utils/translator';
 import loginState from './login-state';
 import LoginWizardPage from './login-wizard-page';
 import ActivityOverlay from '../controls/activity-overlay';
@@ -10,8 +11,8 @@ export default class LoginStart extends LoginWizardPage {
     buttons() {
         return (
             <View>
-                {this.button('login', this.props.login, loginState.isInProgress)}
-                {this.button('signup', () => loginState.routes.app.signupStep1(), loginState.isInProgress)}
+                {this.button('Sign In', this.props.login, loginState.isInProgress)}
+                {this.button('Create Account', () => loginState.routes.app.signupStep1(), loginState.isInProgress)}
                 <ActivityOverlay large visible={loginState.isInProgress} />
             </View>
         );
