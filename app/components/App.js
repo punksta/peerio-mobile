e0694ce64576c0e78276baf10f439200a481cb24
Samvel Avanesov <seavan@gmail.com>
2017-05-18 01:58:50 +0300

    feat(logs): log platform and platform version

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, PanResponder, AppState, ActivityIndicator, NativeModules, BackHandler } from 'react-native';
+import { View, Text, PanResponder, AppState, ActivityIndicator, NativeModules, BackHandler, Platform } from 'react-native';
 import { observer } from 'mobx-react/native';
 import PopupLayout from './layout/popup-layout';
 import ModalLayout from './layout/modal-layout';
@@ -63,6 +63,7 @@ export default class App extends Component {
         this._handleAppStateChange = this._handleAppStateChange.bind(this);
         this._handleMemoryWarning = this._handleMemoryWarning.bind(this);
 
+        console.log(`App.js: ${Platform.OS} ${Platform.Version}`);
         // disable hardware back by default
         BackHandler.addEventListener('hardwareBackPress', () => true);
     }
