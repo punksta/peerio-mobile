3319e90d9d71e739aca2c717bfe422cc35738977
Samvel Avanesov <seavan@gmail.com>
2017-05-20 01:04:33 +0300

    chore(push): refactor

--- app/lib/push.js
+++ app/lib/push.js
@@ -1,28 +1,30 @@
 import PushNotification from 'react-native-push-notification';
 import { Platform } from 'react-native';
-import { when } from 'mobx';
+import { when, reaction, observable } from 'mobx';
 import { socket } from '../lib/icebear';
 
-const TEST_TOKEN = 'a41f1b2e8e9279c81dd9c69c56fd060d25c743354a146d4d9dcddcd9bf73b0e6';
-let registered = false;
+const pushState = observable({
+    registered: false,
+    enabled: true
+});
 
 function onRegister(token) {
     console.log(`push.js: OS: ${Platform.OS}, TOKEN: ${token.token}`);
     const payload = {};
     payload[Platform.OS] = token.token || token;
-    const registerPush = () => {
+    reaction(() => socket.authenticated, authenticated => {
+        if (!authenticated) {
+            console.log(`push.js: unregistered`);
+            pushState.registered = false;
+            return;
+        }
         console.log(`push.js: sending registration OS: ${JSON.stringify(payload)}`);
-        return socket.send('/auth/mobile-device/register', payload)
+        socket.send('/auth/mobile-device/register', payload)
         .then(r => {
-            registered = true;
             console.log(`push.js: register result success ${JSON.stringify(r)}`);
-            when(() => !socket.authenticated, () => (registered = false));
+            pushState.registered = true;
         })
         .catch(e => console.log('push.js: error registering', e));
-    };
-    when(() => socket.authenticated, () => {
-        registerPush();
-        socket.onAuthenticated(registerPush);
     });
 }
 
@@ -48,24 +50,26 @@ function enablePushNotifications() {
     });
 }
 
-function enableServerSide() {
-    when(() => registered && socket.authenticated, () =>
-         socket.send('/auth/push/enable')
-             .then(r => console.log(`push.js: enabled server ${r}`))
-             .catch(e => console.error(e))
-        );
+function toggleServerSide(enable) {
+    const action = enable ? 'enable' : 'disable';
+    console.log(`push.js: ${action} push waiting for socket`);
+    pushState.enabled = enable;
+    when(() => pushState.registered && socket.authenticated, () => {
+        if (enable !== pushState.enabled) return;
+        console.log(`push.js: ${action} push request`);
+        const req = `/auth/push/${action}`;
+        socket.send(req)
+            .then(r => console.log(`push.js: ${action} server ${r}`))
+            .catch(e => console.error(e));
+    });
 }
 
-function disableServerSide() {
-    when(() => registered && socket.authenticated, () =>
-         socket.send('/auth/push/disable')
-             .then(r => console.log(`push.js: disabled server ${r}`))
-             .catch(e => console.error(e))
-        );
-}
+const enableServerSide = () => toggleServerSide(true);
+const disableServerSide = () => toggleServerSide(false);
 
-// if (__DEV__) {
-//     onRegister({ token: TEST_TOKEN });
-// }
+if (__DEV__) {
+    const TEST_TOKEN = 'a41f1b2e8e9279c81dd9c69c56fd060d25c743354a146d4d9dcddcd9bf73b0e6';
+    onRegister({ token: TEST_TOKEN });
+}
 
 module.exports = { enablePushNotifications, enableServerSide, disableServerSide };
