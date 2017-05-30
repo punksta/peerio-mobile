2921affad994848b40759f294e823c54ddfd3117
Samvel Avanesov <seavan@gmail.com>
2017-05-30 17:58:33 +0300

    feat(android): new push notification icon

--- scripts/copy-android-resources.sh
+++ scripts/copy-android-resources.sh
@@ -5,4 +5,8 @@ cp -r app/sounds/*.mp3 android/app/src/main/res/raw
 cp -r locales android/app/src/main/assets/
 cp -r node_modules/peerio-copy/phrase/dict android/app/src/main/assets/
 cp app/assets/terms.txt android/app/src/main/assets/
+cp app/assets/push.png android/app/src/main/res//drawable-xxhdpi/push.png
+cp app/assets/push.png android/app/src/main/res//drawable-xhdpi/push.png
+cp app/assets/push.png android/app/src/main/res//drawable-hdpi/push.png
+cp app/assets/push.png android/app/src/main/res//drawable-mdpi/push.png
 
