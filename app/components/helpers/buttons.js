d48d2238de2bd67ec337b3cf2f529043694b3b85
Samvel Avanesov <seavan@gmail.com>
2017-06-03 23:17:38 +0300

    chore(helpers): uppercase blue button

---
+++ app/components/helpers/buttons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { vars } from '../../styles/styles';
+import { tu } from '../utils/translator';
+
+export default {
+    uppercaseBlueButton(text, onPress, disabled) {
+        return (
+            <TouchableOpacity
+                onPress={disabled ? null : onPress}
+                pressRetentionOffset={vars.pressRetentionOffset}
+                style={{ paddingRight: 12, paddingVertical: 10 }}>
+                <Text style={{ fontWeight: 'bold', color: disabled ? vars.txtMedium : vars.bg }}>
+                    {tu(text)}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+};
