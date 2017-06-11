93cbf72e9b8415949f59aa335bcf004b309a9635
Samvel Avanesov <seavan@gmail.com>
2017-06-12 01:15:19 +0300

    feat(ads): temporarily disabling idfa for current release

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, PanResponder, AppState, ActivityIndicator, NativeModules, Platform, AdSupportIOS } from 'react-native';
+import { View, Text, PanResponder, AppState, ActivityIndicator, NativeModules } from 'react-native';
 import { observer } from 'mobx-react/native';
 import SafeComponent from './shared/safe-component';
 import PopupLayout from './layout/popup-layout';
@@ -63,17 +63,6 @@ export default class App extends SafeComponent {
 
         this._handleAppStateChange = this._handleAppStateChange.bind(this);
         this._handleMemoryWarning = this._handleMemoryWarning.bind(this);
-
-
-        console.log(`App.js: ${NativeModules.AdSupport} ${Platform.OS} ${Platform.Version}`);
-
-        if (Platform.OS === 'ios') {
-            AdSupportIOS.getAdvertisingId(idfa => {
-                console.log(`App.js: tracking ${idfa}`);
-            }, e => {
-                console.log(`App.js: error retrieving idfa, ${e}`);
-            });
-        }
     }
 
     componentDidMount() {
