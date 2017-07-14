10a1f827c4aae30672b45b161bcb6adf644a8361
Samvel Avanesov <seavan@gmail.com>
2017-07-14 11:22:47 -0400

    chore(android): unsign debug builds before resigning

--- scripts/release_android.sh
+++ scripts/release_android.sh
@@ -6,7 +6,6 @@ function signapk(){
   echo ==========================================================
   echo "SIGNING: $1 => $2"
   echo ==========================================================
-  zip -d $1 'META-INF/*.SF' 'META-INF/*.RSA'
   jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore peerio.keystore -storepass:file peerio.keystorepass $1 peerio_release_key
 
   echo ========== VERIFYING ==========
@@ -21,6 +20,8 @@ DATE=`date +%Y-%m-%d`
 signapk ./android/app/build/outputs/apk/app-armeabi-v7a-release-unsigned.apk ./android/app/build/outputs/apk/app-armeabi-v7a-release-signed.apk
 signapk ./android/app/build/outputs/apk/app-x86-release-unsigned.apk ./android/app/build/outputs/apk/app-x86-release-signed.apk
 
+zip -d ./android/app/build/outputs/apk/app-armeabi-v7a-debug.apk 'META-INF/*.SF' 'META-INF/*.RSA'
+zip -d ./android/app/build/outputs/apk/app-x86-debug-unaligned.apk 'META-INF/*.SF' 'META-INF/*.RSA'
 signapk ./android/app/build/outputs/apk/app-armeabi-v7a-debug.apk ./android/app/build/outputs/apk/app-armeabi-v7a-debug-signed.apk
 signapk ./android/app/build/outputs/apk/app-x86-debug-unaligned.apk ./android/app/build/outputs/apk/app-x86-debug-signed.apk
 
