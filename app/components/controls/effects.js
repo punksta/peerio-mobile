4dc1501537944415c2db34e05b772c42a04c599a
Samvel Avanesov <seavan@gmail.com>
2017-07-17 18:37:25 -0400

    feat: new payment ui

--- app/components/controls/effects.js
+++ app/components/controls/effects.js
@@ -4,8 +4,8 @@ import React from 'react';
 import LinearGradient from 'react-native-linear-gradient'; // eslint-disable-line
 import { vars } from '../../styles/styles';
 
-function gradient(props, inner) {
-    props.colors = [vars.bg, vars.bgGradient];
+function gradient(props, inner, leftColor, rightColor) {
+    props.colors = [leftColor || vars.bg, rightColor || vars.bgGradient];
     props.start = { x: 0.0, y: 0.0 };
     props.end = { x: 1.0, y: 0.0 };
     return React.createElement(LinearGradient, props, inner);
@@ -17,9 +17,22 @@ gradient.propTypes = {
     end: PropTypes.any
 };
 
-const effects = {
-    gradient
+function bicolor(props, inner, leftColor, rightColor) {
+    props.colors = [leftColor || vars.bg, rightColor || vars.bgGradient];
+    props.start = { x: 0.5, y: 0.0 };
+    props.end = { x: 0.6, y: 0.0 };
+    return React.createElement(LinearGradient, props, inner);
+}
+
+bicolor.propTypes = {
+    colors: PropTypes.any,
+    start: PropTypes.any,
+    end: PropTypes.any
 };
 
-export { gradient };
+const effects = {
+    gradient, bicolor
+};
+
+export { gradient, bicolor };
 export default effects;
