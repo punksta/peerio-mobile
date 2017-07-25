5b12c621ffbc373c78f4bf7b251d5345e98434f6
Samvel Avanesov <seavan@gmail.com>
2017-07-25 17:32:08 -0400

    fix(ui): refactor text for subscriptions

---
+++ app/components/mocks/mock-channel-create.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { observer } from 'mobx-react/native';
+import ContactList from '../contacts/contact-list';
+import SafeComponent from '../shared/safe-component';
+
+@observer
+export default class MockContactList extends SafeComponent {
+    renderThrow() {
+        return (
+            <View style={{ backgroundColor: 'white', flex: 1, flexGrow: 1 }}>
+                <ContactList />
+            </View>
+        );
+    }
+}
