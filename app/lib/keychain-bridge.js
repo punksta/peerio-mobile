289495afdc8317a88104c68f967b5971638da7d3
Samvel Avanesov <seavan@gmail.com>
2017-08-14 16:57:50 -0400

    chore: better logs for touchid

--- app/lib/keychain-bridge.js
+++ app/lib/keychain-bridge.js
@@ -25,7 +25,8 @@ class KeychainBridge {
         console.log(`keychain-bridge.js: requesting ${key}`);
         return RNKeychain.getValue(key).catch(e => {
             console.log(`keychain-bridge.js: returned error from ${key}`);
-            console.log(e);
+            console.log(e.message);
+            console.log(e.stack);
             return Promise.resolve(null);
         });
     }
