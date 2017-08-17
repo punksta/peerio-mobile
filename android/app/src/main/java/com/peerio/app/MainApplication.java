75216d3dbff188f5cb8c599bf010fc9d2238e260
Samvel Avanesov <seavan@gmail.com>
2017-08-17 18:33:28 -0400

    chore: indent

--- android/app/src/main/java/com/peerio/app/MainApplication.java
+++ android/app/src/main/java/com/peerio/app/MainApplication.java
@@ -47,7 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new FilePickerPackage(),
+                    new FilePickerPackage(),
                     new PickerPackage(),
                     new ReactNativeContacts(),
                     new RNSodiumPackage(),
