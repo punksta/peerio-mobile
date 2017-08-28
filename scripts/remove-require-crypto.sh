e9363123d7990ae9fd7dd5d6bb492ef72a9b625d
Samvel Avanesov <seavan@gmail.com>
2017-08-28 02:43:05 -0400

    chore: remove crypto reference from tweetnacl on build

--- scripts/remove-require-crypto.sh
+++ scripts/remove-require-crypto.sh
@@ -2,3 +2,5 @@
 echo "Removing require('crypto') references"
 perl -pi -e 's/require\(.crypto.\)/null/g' app/lib/peerio-icebear/crypto/util.random.js
 perl -pi -e 's/require\(.crypto.\)/null/g' node_modules/sjcl/sjcl.js
+perl -pi -e 's/require\(.crypto.\)/null/g' node_modules/tweetnacl/nacl-fast.js
+
