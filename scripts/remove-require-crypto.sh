02b5c2e6e84bb0db5192e1f3fc459ea070e9bd00
Samvel Avanesov <seavan@gmail.com>
2017-08-28 02:26:57 -0400

    chore: remove rn-nodeify

---
+++ scripts/remove-require-crypto.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Removing require('crypto') references"
+perl -pi -e 's/require\(.crypto.\)/null/g' app/lib/peerio-icebear/crypto/util.random.js
+perl -pi -e 's/require\(.crypto.\)/null/g' node_modules/sjcl/sjcl.js
