7cd3d93fc49708d22e6033f0fe9f52cfae8bca71
Samvel Avanesov <seavan@gmail.com>
2017-08-29 01:07:18 -0400

    fix: enable primary email check

--- app/components/settings/account-upgrade-swiper.js
+++ app/components/settings/account-upgrade-swiper.js
@@ -43,7 +43,7 @@ export default class AccountUpgradeSwiper extends Component {
             console.log('account-upgrade-swiper: active plans');
             User.current && console.log(User.current.activePlans);
             if (User.current.addresses.filter(e => e.confirmed).length === 0) {
-                // popupYes('', '', t('error_upgradingAccountNoConfirmedEmail')).then(() => routes.modal.discard());
+                popupYes('', '', t('error_upgradingAccountNoConfirmedEmail')).then(() => routes.modal.discard());
             }
         }
     }
