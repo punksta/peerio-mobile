17df4a6ef997e9e07fa8870e062ff961830cf2bd
Karim <karimchehab94@gmail.com>
2017-09-25 12:58:32 -0400

    Fix: setup script now correctly gets IP address of host for Linux and Mac systems
    Other systems are not supported yet

--- scripts/setup-rn-debugging.sh
+++ scripts/setup-rn-debugging.sh
@@ -2,7 +2,29 @@
 echo "Patching RN to disable automatic package launching"
 perl -pi -e 's/RCT_NO_LAUNCH_PACKAGER/MAC_OS_X_PRODUCT_BUILD_VERSION/g' node_modules/react-native/React/React.xcodeproj/project.pbxproj
 echo "Patching RN to enable device debugging"
-ip="$(ifconfig $(route -n get default | awk '/interface: / {print $NF}') | awk '/inet / {print $2}')"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     
+        machine=Linux
+        ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+        ;;
+    Darwin*)    
+        machine=Mac
+        ip="$(ifconfig $(route -n get default | awk '/interface: / {print $NF}') | awk '/inet / {print $2}')"
+        ;;
+#    CYGWIN*)    
+#        machine=Cygwin
+#        ;;
+#    MINGW*)    
+#        machine=MinGw
+#        ;;
+#    *)          
+#        machine="UNKNOWN:${unameOut}"
+#        ;;
+esac
+echo "Machine is ${machine}"
 echo "Local ip: $ip"
+
 sed -i.bak "s/host = @.*/host = @\"$ip\";/" node_modules/react-native/Libraries/WebSocket/RCTWebSocketExecutor.m
 sed -i.bak "s/host = ipGuess \?: @.*/host = ipGuess ?: @\"$ip\";/" node_modules/react-native/React/Base/RCTBundleURLProvider.m
