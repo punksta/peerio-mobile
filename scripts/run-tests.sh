5fa2d24d39ac8563edcad373cea9600effb65c36
Sam Avanesov <seavan@gmail.com>
2017-09-26 08:33:30 -0400

    feat: make android tests work on Linux

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-export PEERIO_IOS_SIM='iPhone 6'
-export PEERIO_IOS_VERSION='10.2'
-SIM_UDID=`xcrun instruments -s | grep "$PEERIO_IOS_SIM ($PEERIO_IOS_VERSION)" | grep -o "\[.*\]" | tr -d '[]'`
-echo $SIM_UDID
-SIM_LOG="$HOME/Library/Logs/CoreSimulator/$SIM_UDID/system.log"
-echo "Logs located:"
-ls $SIM_LOG
+# export PEERIO_IOS_SIM='iPhone 6'
+# export PEERIO_IOS_VERSION='10.2'
+# SIM_UDID=`xcrun instruments -s | grep "$PEERIO_IOS_SIM ($PEERIO_IOS_VERSION)" | grep -o "\[.*\]" | tr -d '[]'`
+# echo $SIM_UDID
+# SIM_LOG="$HOME/Library/Logs/CoreSimulator/$SIM_UDID/system.log"
+# echo "Logs located:"
+# ls $SIM_LOG
 virtualenv .pyenv && source .pyenv/bin/activate
-npm run build-ios-sim-debug
-py.test --platform=ios -s -x tests
+# npm run build-ios-sim-debug
+py.test --platform=android -s -x tests
 deactivate
 
 if [ -z $"$CIRCLE_TEST_REPORTS" ]; then
