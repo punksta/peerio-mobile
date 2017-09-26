884b850e2cd8e72194b59b168100fa54437b0340
Sam Avanesov <seavan@gmail.com>
2017-09-26 16:39:42 -0400

    WIP: android tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,12 +7,25 @@
 # echo "Logs located:"
 # ls $SIM_LOG
 killall appium
+killall node
+sleep 1
 ./node_modules/.bin/appium &
+
+echo "Waiting appium to launch on 4723..."
+
+while ! nc -z localhost 4723; do   
+  sleep 0.1
+done
+
+echo "Appium launched"
+
 adb install android/app/build/outputs/apk/app-x86-debug.apk
 virtualenv .pyenv && source .pyenv/bin/activate
 # npm run build-ios-sim-debug
 py.test --platform=android -s -x tests
 deactivate
+
+killall appium
 
 if [ -z $"$CIRCLE_TEST_REPORTS" ]; then
   exit 0
