40a8bd35dbcc22cbcad897b56d6550bd96180abb
Samvel Avanesov <seavan@gmail.com>
2017-09-27 17:30:29 -0400

    fix(tests): fix tests for new signup process

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -2,16 +2,6 @@ import common.testcase
 import common.helper
 from time import sleep, strftime
 
-def pinEnter():
-    d = common.helper.getDriver()
-    d.tap('pin1')
-    d.tap('pin2')
-    d.tap('pin5')
-    d.tap('pin1')
-    d.tap('pin2')
-    d.tap('pin5')
-    sleep(1)
-
 class Signup(common.testcase.TestCase):
     def test_01_signup(self):
         username = 't' + strftime("%Y%m%d%H%M%S")
@@ -19,25 +9,32 @@ class Signup(common.testcase.TestCase):
         d.reset()
         d.screen('signup-step-a.png')
         # push notification enable alert
-        # d.wait_for_find('Allow')
-        # d.tap('Allow')
-        # d.wait_for_not_find('Allow')
+        try:
+            d.wait_for_find('Allow', 5)
+            d.tap('Allow')
+            d.wait_for_not_find('Allow')
+        except:
+            print 'no push allow alert'
 
         sleep(1)
         d.screen('signup-step-0.png')
+        d.wait_for_find('button_login')
         d.tap('button_CreateAccount')
         sleep(2)
         d.screen('signup-step-1.png')
-        d.send_keys('username', username)
-        d.send_keys('email', username + '@email.com')
         d.send_keys('firstName', 'First')
         d.send_keys('lastName', 'Last')
+        d.send_keys('username', username)
+        d.send_keys('email', username + '@email.com\n')
         sleep(5)
-        d.tap('signupStep1Title')
-        sleep(5)
-        d.tap('signupNext')
+        # d.tap('signupStep1Title')
+        # sleep(5)
+        d.tap('button_next')
         d.screen('signup-step-2.png')
         sleep(5)
-        d.tap('signupNext')
+        d.tap('button_next')
+        d.send_keys('confirmText', 'i have saved my account key')
+        sleep(1)
+        d.tap('button_finish')
         d.screen('signup-step-3.png')
 
