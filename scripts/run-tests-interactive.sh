89ee14a3b38018e066bd09a6809c27b5ba4a1811
Sam Avanesov <seavan@gmail.com>
2017-09-27 17:58:23 -0400

    WIP: android tests

--- scripts/run-tests-interactive.sh
+++ scripts/run-tests-interactive.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-export PEERIO_IOS_SIM='iPhone 6'
-export PEERIO_IOS_VERSION='10.2'
 
-virtualenv .pyenv
-source .pyenv/bin/activate
-# npm run build-ios-sim-debug
+killall appium
+killall node
+sleep 1
+./node_modules/.bin/appium &
+
+echo "Waiting appium to launch on 4723..."
+
+while ! nc -z localhost 4723; do   
+  sleep 0.1
+done
+
+echo "Appium launched"
+
+adb install -r android/app/build/outputs/apk/app-x86-debug.apk
+virtualenv .pyenv && source .pyenv/bin/activate
+
 if [ -z $"PEERIO_TEST" ]; then
   exit 0
   python -i tests/interactive.py 
