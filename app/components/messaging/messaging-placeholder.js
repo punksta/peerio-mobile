2f45c559e99f2d12751c1e5c28f2ed07720c004a
Lucas <lucas@superko.org>
2017-10-02 19:08:50 -0400

    feat: new zero chat design

--- app/components/messaging/messaging-placeholder.js
+++ app/components/messaging/messaging-placeholder.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { observer } from 'mobx-react/native';
 import { View, Text, Dimensions, Image } from 'react-native';
 import SafeComponent from '../shared/safe-component';
-import { tx } from '../utils/translator';
-
-const chatZeroState = require('../../assets/chat-zero-state.png');
+import { T } from '../utils/translator';
+import BgPattern from '../controls/bg-pattern';
+import { vars } from '../../styles/styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 @observer
 export default class MessagingPlaceholder extends SafeComponent {
@@ -31,54 +32,42 @@ export default class MessagingPlaceholder extends SafeComponent {
             flexGrow: 1,
             alignItems: 'stretch'
         };
-        const titleBlockStyle = {
-            flex: 0,
-            height: 80,
-            flexDirection: 'row',
-            alignItems: 'center'
+
+        const inpagePopupStyle = {
+            justifyContent: 'center',
+            borderRadius: 8,
+            borderTopWidth: 6,
+            shadowColor: '#000000',
+            shadowOpacity: 0.2,
+            shadowRadius: 8,
+            shadowOffset: {
+                height: 1,
+                width: 1
+            },
+            elevation: 10,
+            margin: 20,
+            padding: 20,
+            backgroundColor: 'white'
         };
-        const textHintContainer = {
-            flex: 0.3,
-            alignItems: 'center',
-            marginTop: 20,
-            marginBottom: 40,
-            justifyContent: 'space-between'
+        const title = {
+            fontWeight: 'bold',
+            fontSize: 16,
+            marginBottom: 8
         };
+
+        const textParser = {
+            emphasis: text => <Bold>{"hi"}</Bold>,
+            plusIcon: text => (
+                <Icon name="add" />
+            )
+        };
+
         return (
             <View style={outerContainerStyle}>
-                <View style={[titleBlockStyle, { marginTop: 16 }]}>
-                    <Text style={headerStyle}>
-                        {tx('title_haveAConversation')}
-                    </Text>
-                </View>
-                <View style={{
-                    borderColor: 'red',
-                    borderWidth: 0,
-                    flexGrow: 0,
-                    alignSelf: 'center',
-                    justifyContent: 'center',
-                    paddingLeft: 20,
-                    paddingRight: 20,
-                    width: 245,
-                    height: 232,
-                    marginTop: 32
-                }}>
-                    <Image source={chatZeroState}
-                        resizeMode="contain"
-                        style={{ flexGrow: 1, width: null, height: null }} />
-                </View>
-                <View style={textHintContainer}>
-                    <View style={{ flex: 1 }}>
-                        <Text style={infoStyle}>
-                            {tx('title_directMessages')}
-                        </Text>
-                        <Text style={infoStyle}>
-                            {tx('title_channels')}
-                        </Text>
-                        <Text style={infoStyle}>
-                            {tx('title_uploadShareAndManage')}
-                        </Text>
-                    </View>
+                <BgPattern />
+                <View style={inpagePopupStyle}>
+                    <Text style={title}><T k="title_welcomeHeading"></T></Text>
+                    <T k="dialog_chatZeroState">{textParser}</T>
                 </View>
             </View>
         );
