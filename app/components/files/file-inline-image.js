1d9d45cd911bee9ac2b02e7a85c4331271b33d09
Samvel Avanesov <seavan@gmail.com>
2017-10-05 15:37:27 -0400

    WIP: inline images

--- app/components/files/file-inline-image.js
+++ app/components/files/file-inline-image.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { observer } from 'mobx-react/native';
+import { observable } from 'mobx';
 import { View, Image, Text } from 'react-native';
 import SafeComponent from '../shared/safe-component';
 
+const OPTIMAL_WIDTH = 300;
+const OPTIMAL_HEIGHT = 200;
+
 @observer
 export default class FileInlineImage extends SafeComponent {
+    @observable width = 10;
+    @observable height = 10;
+
+    componentWillMount() {
+        Image.getSize(this.props.image, (width, height) => {
+
+        });
+    }
+
     renderThrow() {
         const { image } = this.props;
+        const { width, height } = this;
         const source = { uri: image };
         return (
             <View>
-                <Text>{image}</Text>
-                <Image source={source} style={{ width: 100, height: 100 }} />
+                <Image source={source} style={{ width, height }} />
             </View>
         );
     }
