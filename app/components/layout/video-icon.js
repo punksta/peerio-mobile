2b60a3e422e58fb0300c30bd8407d55fdd4dc150
Gem Barrett <peeriogem@Gems-MacBook-Pro-3.local>
2017-10-18 18:58:30 -0400

    App store URL is now assigned based on OS

--- app/components/layout/video-icon.js
+++ app/components/layout/video-icon.js
@@ -1,6 +1,13 @@
+import { Platform } from 'react-native';
 import HeaderIconBase from './header-icon-base';
+// import { vars } from '../../styles/styles';
+
+const androidJitsi = 'https://play.google.com/store/apps/details?id=org.jitsi.meet';
+const iosJitsi = 'https://itunes.apple.com/app/jitsi-meet/id1165103905';
+const storeURL = Platform.OS === 'android' ? androidJitsi : iosJitsi;
 
 export default class VideoIcon extends HeaderIconBase {
     icon = 'videocam';
-    action = () => this.props.action();
+    // style = { borderTopColor: vars.yellowLine, borderTopWidth: 8 };
+    action = () => this.props.action(console.log('the store is ', storeURL));
 }
