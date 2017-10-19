593c474ccbd7a1ad84c239030af4e42d03f8a448
Karim94 <karimchehab94@gmail.com>
2017-10-19 18:57:47 -0400

    fixed cropped inline image container

--- app/components/files/file-inline-image.js
+++ app/components/files/file-inline-image.js
@@ -171,13 +171,16 @@ export default class FileInlineImage extends SafeComponent {
         };
 
         const header = {
+            flexGrow: 1,
+            flexShrink: 1,
             flexDirection: 'row',
-            alignItems: 'center',
             justifyContent: 'space-between',
             marginBottom: !downloading && this.opened ? 10 : 0
         };
 
         const text = {
+            flexGrow: 1,
+            flexShrink: 1,
             fontWeight: 'bold',
             color: vars.txtMedium
         };
