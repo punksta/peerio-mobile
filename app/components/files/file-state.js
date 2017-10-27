c06d2e7a72563318f84d273b3e466ded6b1d350a
Samvel Avanesov <seavan@gmail.com>
2017-10-26 22:05:31 -0400

    Revert "Revert "perf: sdk calls loadAllFiles now as late as possible to speed up startup""
    This reverts commit 01797734fce2c13b3c6dcea2bd9aaf5f048e069c.

--- app/components/files/file-state.js
+++ app/components/files/file-state.js
@@ -15,7 +15,6 @@ class FileState extends RoutedState {
     _prefix = 'files';
 
     @action async init() {
-        this.store.loadAllFiles();
         return new Promise(resolve => when(() => !this.store.loading, resolve));
     }
 
@@ -144,7 +143,6 @@ class FileState extends RoutedState {
 
     onTransition(active, file) {
         console.log('files on transition');
-        active && fileStore.loadAllFiles();
         clientApp.isInFilesView = active && !!file;
         this.currentFile = active ? file : null;
     }
