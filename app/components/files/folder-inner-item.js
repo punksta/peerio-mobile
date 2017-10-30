6c1bb4e92c6e4435dd8a4a8603f64d020e05f382
Samvel Avanesov <seavan@gmail.com>
2017-10-30 12:47:56 -0400

    chore: cleanup checkbox because we are not currently using it

--- app/components/files/folder-inner-item.js
+++ app/components/files/folder-inner-item.js
@@ -1,26 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { Text, Dimensions, View } from 'react-native';
+import { Text, View } from 'react-native';
 import moment from 'moment';
 import SafeComponent from '../shared/safe-component';
 import { vars } from '../../styles/styles';
 import icons from '../helpers/icons';
 
-const width = Dimensions.get('window').width;
-const height = 64;
-const checkBoxWidth = height;
+const height = vars.listItemHeight;
 const itemContainerStyle = {
     flex: 1,
     flexGrow: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(0, 0, 0, .12)',
     backgroundColor: 'white',
     height,
-    width,
     borderWidth: 0,
     borderColor: 'red',
     paddingLeft: vars.spacing.small.midi2x
@@ -28,41 +23,12 @@ const itemContainerStyle = {
 
 const folderInfoContainerStyle = {
     flexGrow: 1,
-    flexDirection: 'row',
-    borderWidth: 0,
-    borderColor: 'green'
+    flexDirection: 'row'
 };
 
 @observer
 export default class FolderInnerItem extends SafeComponent {
-    onPress() {
-        const folder = this.props.folder;
-        this.props.onPress ? this.props.onPress(this.props.folder)
-            : (folder.selected = !folder.selected);
-    }
-
-    checkbox() {
-        const checked = this.props.folder && this.props.folder.selected;
-        console.log(`checked: ${checked}`);
-        const v = vars;
-        const color = checked ? v.checkboxActive : v.checkboxInactive;
-        const iconColor = checked ? 'white' : v.checkboxIconInactive;
-        const iconBgColor = 'transparent';
-        const icon = checked ? 'check-box' : 'check-box-outline-blank';
-        const outer = {
-            backgroundColor: color,
-            padding: vars.spacing.small.mini2x,
-            flex: 0,
-            width: checkBoxWidth,
-            justifyContent: 'center',
-            alignItems: 'center'
-        };
-        return (
-            <View style={outer} pointerEvents="none">
-                {icons.colored(icon, null, iconColor, iconBgColor)}
-            </View>
-        );
-    }
+    onPress = () => this.props.onPress && this.props.onPress(this.props.folder);
 
     renderThrow() {
         const folder = this.props.folder;
@@ -79,7 +45,6 @@ export default class FolderInnerItem extends SafeComponent {
         let icon = 'folder';
         icon = icons.dark(icon);
         const loadingStyle = null;
-        const marginLeft = this.props.checkbox === 'always' ? 0 : -checkBoxWidth;
         const arrow = this.props.hideArrow ? null : (
             <View style={{ flex: 0 }}>
                 {icons.dark('keyboard-arrow-right', this.onPress)}
@@ -87,9 +52,8 @@ export default class FolderInnerItem extends SafeComponent {
         );
         return (
             <View style={{ backgroundColor: 'white' }}>
-                <View style={[folderInfoContainerStyle, { marginLeft }]}>
-                    {this.checkbox()}
-                    <View style={[itemContainerStyle, { width: width - marginLeft - checkBoxWidth }]}>
+                <View style={folderInfoContainerStyle}>
+                    <View style={itemContainerStyle}>
                         <View style={[loadingStyle, { flex: 0 }]}>
                             {icon}
                         </View>
@@ -110,6 +74,5 @@ export default class FolderInnerItem extends SafeComponent {
 FolderInnerItem.propTypes = {
     onPress: PropTypes.func,
     folder: PropTypes.any.isRequired,
-    checkbox: PropTypes.string,
     hideArrow: PropTypes.bool
 };
