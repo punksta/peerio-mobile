5c5289b5afa18aa0ce0f95766c2e5758044e69e8
Gem Barrett <peeriogem@Gems-MacBook-Pro-3.local>
2017-11-04 14:12:47 -0400

    Continuing refactor of video call dialog

--- app/components/layout/video-icon.js
+++ app/components/layout/video-icon.js
@@ -2,6 +2,7 @@ import HeaderIconBase from './header-icon-base';
 import { popupSetupVideo } from '../shared/popups';
 import ChatVideo from '../messaging/chat-video';
 import preferenceStore from '../settings/preference-store';
+import { tx } from '../utils/translator';
 
 export default class VideoIcon extends HeaderIconBase {
     icon = 'videocam';
@@ -9,8 +10,13 @@ export default class VideoIcon extends HeaderIconBase {
     // the action assigned to video icon, asynchronously grabs the user preferences
     action = async () => {
         const { prefs } = preferenceStore;
+        const dialog = {
+            titleText: tx('title_videoCall'),
+            subText: tx('dialog_videoCall'),
+            disc: tx('disclaimer_videoCall')
+        };
         // check if you've seen the popup, otherwise call the popup, then add jitsi link to props
-        if (prefs.hasSeenJitsiSuggestionPopup || await popupSetupVideo(ChatVideo.storeLink)) {
+        if (prefs.hasSeenJitsiSuggestionPopup || await popupSetupVideo(dialog.titleText, dialog.subText, dialog.disc, ChatVideo.storeLink)) {
             prefs.hasSeenJitsiSuggestionPopup = true;
             // add function as a prop which then grabs the link when called
             this.props.onAddVideoLink(ChatVideo.jitsiLink);
