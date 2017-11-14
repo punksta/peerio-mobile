3c0b4bde75711bb34ecdcd38a513a8469b6b62a8
Alexandra Marin <alexandra.marin01@gmail.com>
2017-11-14 22:22:10 +0200

    WIP: revert tests

--- __mocks__/rn-file-stream.js
+++
@@ -1,9 +0,0 @@
-const RNFileStream = {};
-
-RNFileStream.open = jest.fn();
-RNFileStream.close = jest.fn();
-RNFileStream.readInternal = jest.fn();
-RNFileStream.seekInternal = jest.fn();
-RNFileStream.writeInternal = jest.fn();
-
-module.exports = RNFileStream;
