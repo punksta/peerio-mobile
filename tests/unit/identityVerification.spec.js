1e567a0ac3302918f1b41c2ac3b5c90ca12a23eb
Alexandra Marin <alexandra.marin01@gmail.com>
2017-11-14 20:29:15 +0200

    WIP: cleanup

--- tests/unit/identityVerification.spec.js
+++ tests/unit/identityVerification.spec.js
@@ -1,27 +1,20 @@
+import 'react-native';
 import React from 'react';
-import { Text, NativeModules } from 'react-native';
 import renderer from 'react-test-renderer';
 import jest from 'jest';
 import IdentityVerificationNotice from '../../app/components/shared/identityVerificationNotice';
 
-// jest.mock('NativeModules', () => {
-//     const React = require('react');
-//     return {
-//         RNFSManager: {
-//             RNFSFileTypeRegular: jest.fn()
-//         }
-//     };
-// });
-
-
-beforeEach(() => {
-    NativeModules.RNFSManager = { RNFSFileTypeRegular: jest.fn(()=>{}) };
-    // require('react-native-fs').__setMockFiles();
+jest.mock('NativeModules', () => {
+    const j = require('jest');
+    return {
+        RNFSManager: {
+            RNFSFileTypeRegular: j.fn()
+        }
+    };
 });
 
 it('renders correctly', () => {
     const tree = renderer.create(
-        // <Text>test</Text>
         <IdentityVerificationNotice />
     ).toJSON();
     expect(tree).toMatchSnapshot();
