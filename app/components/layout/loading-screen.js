2f128e3b5cf945b3b80b25106b695f88bd7a5108
Karim94 <karimchehab94@gmail.com>
2017-11-16 13:43:13 -0500

    MVP animated loading screen

--- app/components/layout/loading-screen.js
+++ app/components/layout/loading-screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Animated, View, Text, Image, Easing } from 'react-native';
 import { observer } from 'mobx-react/native';
+import { observable, computed } from 'mobx';
 import { vars } from '../../styles/styles';
 
 const connectingInProgress = require('../../assets/loading_screens/connecting-inProgress.png');
@@ -22,7 +23,225 @@ const lineDone = require('../../assets/loading_screens/line-done.png');
 
 @observer
 export default class LoadingScreen extends Component {
-    // Maps each state to which asset the image should use
+    @observable loadingStep = 0;
+    fadeValue;
+    growValue;
+    smallIcon;
+    bigIcon;
+
+    constructor(props) {
+        super(props);
+        this.fadeValue = new Animated.Value(0.70);
+        this.growValue = new Animated.Value(1);
+        this.smallIcon = {
+            height: 24,
+            width: 24,
+            marginHorizontal: 6,
+            transform: [{ scale: 1 }]
+        };
+        this.bigIcon = {
+            height: 24,
+            width: 24,
+            marginHorizontal: 6,
+            transform: [{ scale: this.growValue }],
+            opacity: this.fadeValue
+        };
+        // TODO Delete setInterval(.....)
+        setInterval(this.changeState, 2000);
+    }
+
+    componentDidMount() {
+        this.fadeInOut();
+        this.growIcon();
+    }
+
+    // Modify this function to accept input about when loadStep changes
+    changeState = () => {
+        const nextStep = (this.loadingStep === 4) ? 0 : this.loadingStep + 1;
+        this.loadingStep = nextStep;
+        this.growIcon();
+    }
+
+    fadeInOut() {
+        Animated.sequence([
+            Animated.timing(
+                this.fadeValue,
+                {
+                    toValue: 1,
+                    duration: 400,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                }),
+            Animated.timing(
+                this.fadeValue,
+                {
+                    toValue: 0.70,
+                    duration: 400,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                })
+        ]).start(() => this.fadeInOut());
+    }
+
+    growIcon() {
+        Animated.timing(
+            this.growValue,
+            {
+                toValue: 1.5,
+                duration: 300,
+                easing: Easing.linear,
+                useNativeDriver: true
+            }).start();
+    }
+
+    getSource(imageName) {
+        return this.images[this.loadingStep][imageName];
+    }
+
+    @computed get connectingIconStyle() {
+        if (this.loadingStep === 0) return this.bigIcon;
+        return this.smallIcon;
+    }
+
+    @computed get authenticatingIconStyle() {
+        if (this.loadingStep === 1) return this.bigIcon;
+        return this.smallIcon;
+    }
+
+    @computed get decryptingIconStyle() {
+        if (this.loadingStep === 2) return this.bigIcon;
+        return this.smallIcon;
+    }
+
+    @computed get confirmingIconStyle() {
+        if (this.loadingStep === 3) return this.bigIcon;
+        return this.smallIcon;
+    }
+
+    @computed get doneIconStyle() {
+        if (this.loadingStep === 4) return this.bigIcon;
+        return this.smallIcon;
+    }
+
+    getStatusText() {
+        switch (this.loadingStep) {
+            case (0):
+                return 'Connecting...';
+            case (1):
+                return 'Authenticating...';
+            case (2):
+                return 'Decrypting...';
+            case (3):
+                return 'Confirming data...';
+            case (4):
+                return 'Done!';
+            default:
+                return 'Connecting...';
+        }
+    }
+
+    render() {
+        const container = {
+            backgroundColor: 'white',
+            flex: 1,
+            flexGrow: 1,
+            alignItems: 'center'
+        };
+        const flavorTextStyle = {
+            fontSize: vars.font.size.big,
+            color: vars.subtleText,
+            paddingHorizontal: 24,
+            textAlign: 'center',
+            marginTop: vars.spacing.huge.maxi2x * 2.75
+        };
+        const loadingProgressContainer = {
+            flex: 1,
+            flexGrow: 1,
+            justifyContent: 'flex-end',
+            marginBottom: vars.spacing.huge.maxi2x * 2.25
+        };
+        const iconContainer = {
+            flexDirection: 'row',
+            paddingHorizontal: 40,
+            height: 48,
+            justifyContent: 'center',
+            alignItems: 'center'
+        };
+        const statusTextStyle = {
+            marginTop: 20,
+            fontSize: vars.font.size.big,
+            color: vars.subtleText,
+            textAlign: 'center'
+        };
+        const lineStyle = {
+            height: 3,
+            width: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginHorizontal: 5
+        };
+        return (
+            <View style={container}>
+                <Text style={flavorTextStyle}>
+                    Lorem Ipsum dolores sit resgtihm. orleoro colares ip
+                </Text>
+                <View style={loadingProgressContainer}>
+                    <View style={iconContainer}>
+                        <Animated.Image
+                            source={this.getSource('connectingIcon')}
+                            style={this.connectingIconStyle}
+                            resizeMode="contain"
+                        />
+                        <Image
+                            source={this.getSource('connectingLine')}
+                            style={lineStyle}
+                            resizeMode="contain"
+                        />
+                        <Animated.Image
+                            source={this.getSource('authenticatingIcon')}
+                            style={this.authenticatingIconStyle}
+                            resizeMode="contain"
+                        />
+                        <Image
+                            source={this.getSource('authenticatingLine')}
+                            style={lineStyle}
+                            resizeMode="contain"
+                        />
+                        <Animated.Image
+                            source={this.getSource('decryptingIcon')}
+                            style={this.decryptingIconStyle}
+                            resizeMode="contain"
+                        />
+                        <Image
+                            source={this.getSource('decryptingLine')}
+                            style={lineStyle}
+                            resizeMode="contain"
+                        />
+                        <Animated.Image
+                            source={this.getSource('confirmingIcon')}
+                            style={this.confirmingIconStyle}
+                            resizeMode="contain"
+                        />
+                        <Image
+                            source={this.getSource('confirmingLine')}
+                            style={lineStyle}
+                            resizeMode="contain"
+                        />
+                        <Animated.Image
+                            source={this.getSource('doneIcon')}
+                            style={this.doneIconStyle}
+                            resizeMode="contain"
+                        />
+                    </View>
+                    <Text style={statusTextStyle}>
+                        {this.getStatusText()}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+
+    // Maps each Loading Step to the assets that each image should use
     images = {
         // Loading Step
         0: {
@@ -87,194 +306,4 @@ export default class LoadingScreen extends Component {
             doneLine: lineInProgress
         }
     };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            loadingStep: 3
-        };
-        this.fadeValue = new Animated.Value(0.80);
-        this.growValue = new Animated.Value(24);
-        // TODO Delete setInterval(.....)
-        setInterval(this.changeState, 2000);
-    }
-
-    componentDidMount() {
-        this.fadeInOut();
-        this.growIcon();
-    }
-
-    // Delete this function
-    changeState = () => {
-        console.log('State changed');
-        const nextStep = (this.state.loadingStep === 4) ? 0 : this.state.loadingStep + 1;
-        this.setState({
-            loadingStep: nextStep
-        });
-    }
-
-    fadeInOut() {
-        console.log('Fading');
-        Animated.sequence([
-            Animated.timing(
-                this.fadeValue,
-                {
-                    toValue: 1,
-                    duration: 500,
-                    easing: Easing.linear
-                }),
-            Animated.timing(
-                this.fadeValue,
-                {
-                    toValue: 0.80,
-                    duration: 500,
-                    easing: Easing.linear
-                })
-        ]).start(() => this.fadeInOut());
-    }
-
-    growIcon() {
-        console.log('Growing');
-        Animated.timing(
-            this.growValue,
-            {
-                toValue: 36,
-                duration: 300,
-                easing: Easing.linear
-            }).start();
-    }
-
-    getSource(imageName) {
-        return this.images[this.state.loadingStep][imageName];
-    }
-
-    getIconStyle(imageName) {
-        const smallIcon = { height: 24, width: 24, marginHorizontal: 6 };
-        const bigIcon = { height: this.growValue, width: this.growValue, opacity: this.fadeValue };
-        if (this.state.loadingStep === 0 && imageName === 'connectingIcon') return bigIcon;
-        else if (this.state.loadingStep === 1 && imageName === 'authenticatingIcon') return bigIcon;
-        else if (this.state.loadingStep === 2 && imageName === 'decryptingIcon') return bigIcon;
-        else if (this.state.loadingStep === 3 && imageName === 'confirmingIcon') return bigIcon;
-        else if (this.state.loadingStep === 4 && imageName === 'doneIcon') return bigIcon;
-        return smallIcon;
-    }
-
-    getStatusText() {
-        switch (this.state.loadingStep) {
-            case (0):
-                return 'Connecting...';
-            case (1):
-                return 'Authenticating...';
-            case (2):
-                return 'Decrypting...';
-            case (3):
-                return 'Confirming data...';
-            case (4):
-                return 'Done!';
-            default:
-                return 'Connecting...';
-        }
-    }
-
-    render() {
-        this.step = this.state.step;
-        const container = {
-            backgroundColor: 'white',
-            flex: 1,
-            flexGrow: 1,
-            alignItems: 'center'
-        };
-        const flavorTextStyle = {
-            fontSize: vars.font.size.big,
-            color: vars.subtleText,
-            paddingHorizontal: 24,
-            textAlign: 'center',
-            marginTop: vars.spacing.huge.maxi2x * 2.75
-        };
-        const loadingProgressContainer = {
-            flex: 1,
-            flexGrow: 1,
-            justifyContent: 'flex-end',
-            marginBottom: vars.spacing.huge.maxi2x * 2.25
-        };
-        const iconContainer = {
-            flexDirection: 'row',
-            paddingHorizontal: 40,
-            height: 48,
-            justifyContent: 'center',
-            alignItems: 'center'
-        };
-        const statusTextStyle = {
-            marginTop: 20,
-            fontSize: vars.font.size.big,
-            color: vars.subtleText,
-            textAlign: 'center'
-        };
-        const lineStyle = {
-            height: 3,
-            width: 20,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginHorizontal: 5
-        };
-        return (
-            <View style={container}>
-                <Text style={flavorTextStyle}>
-                    Lorem Ipsum dolores sit resgtihm. orleoro colares ip
-                </Text>
-                <View style={loadingProgressContainer}>
-                    <View style={iconContainer}>
-                        <Animated.Image
-                            source={this.getSource('connectingIcon')}
-                            style={this.getIconStyle('connectingIcon')}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={this.getSource('connectingLine')}
-                            style={lineStyle}
-                            resizeMode="contain"
-                        />
-                        <Animated.Image
-                            source={this.getSource('authenticatingIcon')}
-                            style={this.getIconStyle('authenticatingIcon')}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={this.getSource('authenticatingLine')}
-                            style={lineStyle}
-                            resizeMode="contain"
-                        />
-                        <Animated.Image
-                            source={this.getSource('decryptingIcon')}
-                            style={this.getIconStyle('decryptingIcon')}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={this.getSource('decryptingLine')}
-                            style={lineStyle}
-                            resizeMode="contain"
-                        />
-                        <Animated.Image
-                            source={this.getSource('confirmingIcon')}
-                            style={this.getIconStyle('confirmingIcon')}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={this.getSource('confirmingLine')}
-                            style={lineStyle}
-                            resizeMode="contain"
-                        />
-                        <Animated.Image
-                            source={this.getSource('doneIcon')}
-                            style={this.getIconStyle('doneIcon')}
-                            resizeMode="contain"
-                        />
-                    </View>
-                    <Text style={statusTextStyle}>
-                        {this.getStatusText()}
-                    </Text>
-                </View>
-            </View>
-        );
-    }
 }
