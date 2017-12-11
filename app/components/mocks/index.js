dc59c88e265b7da1b461a11d5806776e3eaab631
Samvel Avanesov <seavan@gmail.com>
2017-12-11 01:48:10 -0500

    WIP: circleci 2.0

--- app/components/mocks/index.js
+++ app/components/mocks/index.js
@@ -3,6 +3,7 @@
 // import MockChatList from './mock-chat-list';
 // import MockTwoFactorAuth from './mock-two-factor-auth';
 // import MockPopups from './mock-popups';
+import MockImageError from './mock-image-error';
 
-export default null;
-// export default __DEV__ ? MockPopups : null;
+// export default null;
+export default __DEV__ ? MockImageError : null;
