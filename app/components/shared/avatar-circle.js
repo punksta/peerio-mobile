75ff150e6dd76a894771422b4f7ffc6194ed0e36
Karim94 <Karim94@users.noreply.github.com>
2018-01-19 13:31:38 -0500

    fix: blank avatars when loading contacts (#105)
    * WIP: fix blank avatars [branch ch926]
    
    * WIP: blank avatars
    
    * fix: avatars are now defaulted to grey letters while they load
    
    * fix: refactor render without double using avatar letter

--- app/components/shared/avatar-circle.js
+++ app/components/shared/avatar-circle.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { observable, action } from 'mobx';
 import { observer } from 'mobx-react/native';
 import { Text, ActivityIndicator, View, Image } from 'react-native';
 import SafeComponent from '../shared/safe-component';
@@ -11,6 +12,12 @@ const avatarDiameter = 36;
 
 @observer
 export default class AvatarCircle extends SafeComponent {
+    @observable avatarLoaded;
+
+    @action.bound handleOnLoad() {
+        this.avatarLoaded = true;
+    }
+
     renderThrow() {
         const { large, medium, contact, loading, invited } = this.props;
         let ratio = 1;
@@ -35,27 +42,45 @@ export default class AvatarCircle extends SafeComponent {
             alignItems: 'center',
             backgroundColor: color || 'gray'
         }];
-        let inner = <View style={coloredAvatarStyle}>{icons.plainWhite('group')}</View>;
+        const avatarLetter = (
+            <View style={coloredAvatarStyle}>
+                <Text style={{ color: 'white', textAlign: 'center', width: 14 * ratio, fontSize: vars.font.size.smaller * ratio }}>
+                    {letter}
+                </Text>
+            </View>
+        );
+
+        const groupIcon = <View style={coloredAvatarStyle}>{icons.plainWhite('group')}</View>;
+        let avatarIcon = null;
         if (contact) {
+            if (invited) {
+                avatarIcon = (
+                    <View style={[avatarStyle, { justifyContent: 'center', alignItems: 'center' }]}>
+                        {icons.plaindark('person')}
+                    </View>
+                );
+            }
             if (contact.hasAvatar) {
                 const uri = (large || medium) ? contact.largeAvatarUrl : contact.mediumAvatarUrl;
-                inner = <Image source={{ uri, cache: 'force-cache' }} key={uri} style={avatarStyle} />;
-            } else if (invited) {
-                inner = <View style={[avatarStyle, { justifyContent: 'center', alignItems: 'center' }]}>{icons.plaindark('person')}</View>;
-            } else {
-                inner = (
-                    <View style={coloredAvatarStyle}>
-                        <Text style={{ color: 'white', textAlign: 'center', width: 14 * ratio, fontSize: vars.font.size.smaller * ratio }}>
-                            {letter}
-                        </Text>
-                    </View>
+                const opacity = this.avatarLoaded ? 1 : 0;
+                avatarIcon = (
+                    <Image
+                        source={{ uri, cache: 'force-cache' }}
+                        key={uri}
+                        style={[avatarStyle, { opacity }]}
+                        onLoad={this.handleOnLoad}
+                    />
                 );
             }
         }
 
         return (
             <View style={{ borderWidth: 0, borderColor: 'green' }}>
-                {inner}
+                {/* if we don't have contact specified, show group icon */}
+                {!contact && groupIcon}
+                {avatarIcon}
+                {/* show letter if there's no avatar or it hasn't loaded yet */}
+                {contact && (!avatarIcon || !this.avatarLoaded) && avatarLetter}
                 <ErrorCircle large={this.props.large} visible={tofuError} />
             </View>
         );
