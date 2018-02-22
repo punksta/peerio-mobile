punksta/peerio-mobile

commit 537fe459a224ce079c3bc0ec472e6161ffcaa071

chore: eslint

app/components/settings/preference-store.js
@@ -16,8 +16,7 @@ class PreferenceStore {
         limitInlineImageSize: true, // will use config.chat.inlineImageSizeLimit
         externalContentConsented: false, // false - no feedback from user yet, true - user expressed their desire
         externalContentEnabled: true,
-        externalContentJustForFavs: false,
-        peerioContentEnabled: true,
+        externalContentJustForFavs: false
     };
 
     observePreference(key, dbName, localStore) {

app/components/channels/create-channel.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Dimensions, LayoutAnimation, TextInput, Keyboard } from 'react-native';
+import { View, Text, ScrollView, Dimensions, LayoutAnimation, Keyboard } from 'react-native';
 import { observer } from 'mobx-react/native';
 import { observable, reaction, action } from 'mobx';
 import ContactSelectorUniversal from '../contacts/contact-selector-universal';

app/components/files/file-view.js
@@ -8,7 +8,7 @@ import icons from '../helpers/icons';
 import { vars } from '../../styles/styles';
 import FileProgress from './file-progress';
 import FileActions from './file-actions';
-import { uiState, fileState } from '../states';
+import { fileState } from '../states';
 import { tx } from '../utils/translator';
 import FileTypeIcon from './file-type-icon';
 import { fileHelpers } from '../../lib/icebear';

app/components/files/folder-select.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { View, ListView, Animated, Text } from 'react-native';
+import { View, ListView, Text } from 'react-native';
 import { observable, reaction, computed } from 'mobx';
 import SafeComponent from '../shared/safe-component';
 import FolderInnerItem from './folder-inner-item';

app/components/files/inline-file-action-sheet.js
@@ -14,7 +14,7 @@ export default class InlineFileActionSheet extends SafeComponent {
     sharefile = () => {
         fileState.currentFile = this.file;
         routerModal.shareFileTo();
-    }
+    };
 
     get openItem() {
         const exists = this.file && !this.file.isPartialDownload && this.file.cached;
@@ -44,7 +44,7 @@ export default class InlineFileActionSheet extends SafeComponent {
     show = (file) => {
         this.file = file;
         this._actionSheet.show();
-    }
+    };
 
     renderThrow() {
         return (

app/components/messaging/channel-invite.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { action, observable } from 'mobx';
 import { Text, View } from 'react-native';

app/components/mocks/index.js
@@ -1,4 +1,4 @@
-import MockChannel from './mock-channel';
+// import MockChannel from './mock-channel';
 /* import MockChannelCreate from './mock-channel-create'; */
 // import MockChatList from './mock-chat-list';
 // import MockTwoFactorAuth from './mock-two-factor-auth';

app/components/mocks/mock-chat-list.js
@@ -3,7 +3,7 @@ import { View, StatusBar } from 'react-native';
 import { observer } from 'mobx-react/native';
 import PopupLayout from '../layout/popup-layout';
 import ChatList from '../messaging/chat-list';
-import ChannelInviteList from '../messaging/channel-invite-list';
+import ChannelInvite from '../messaging/channel-invite';
 import { User } from '../../lib/icebear';
 import chatState from '../messaging/chat-state';
 import mockChatStore from './mock-chat-store';
@@ -20,7 +20,7 @@ export default class MockChatList extends Component {
     render() {
         return (
             <View style={{ backgroundColor: 'white', flex: 1, flexGrow: 1, paddingTop: vars.layoutPaddingTop }}>
-                {chatState.routerMain.route === 'channelInviteList' ? <ChannelInviteList /> : <ChatList />}
+                {chatState.routerMain.route === 'channelInviteList' ? <ChannelInvite /> : <ChatList />}
                 <PopupLayout key="popups" />
                 <StatusBar barStyle="default" />
             </View>

commit 7a5d864955c9f072fdd5e889fb75695edb371997

chore: return peerioContentEnabled broken by 537fe459a224ce079c3bc0ec472e6161ffcaa071

app/components/settings/preference-store.js
@@ -16,7 +16,8 @@ class PreferenceStore {
         limitInlineImageSize: true, // will use config.chat.inlineImageSizeLimit
         externalContentConsented: false, // false - no feedback from user yet, true - user expressed their desire
         externalContentEnabled: true,
-        externalContentJustForFavs: false
+        externalContentJustForFavs: false,
+        peerioContentEnabled: true
     };
 
     observePreference(key, dbName, localStore) {