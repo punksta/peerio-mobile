faf6e8740c31189fc5cfd7503391ca5615c4ff96
Samvel Avanesov <seavan@gmail.com>
2018-03-12 16:59:51 -0400

    fix: fileUploadActionSheet

--- app/components/files/files.js
+++ app/components/files/files.js
@@ -48,7 +48,7 @@ export default class Files extends SafeComponent {
     get rightIcon() {
         return !fileState.isFileSelectionMode &&
             <PlusBorderIcon
-                action={() => filesUploadActionSheet.show()}
+                action={() => fileUploadActionSheet.show()}
                 testID="buttonUploadFileToFiles" />;
     }
 
