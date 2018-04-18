ba144d77142a6bacc7e7249a310e498757d947de
Alexandra Marin <alexandra.marin01@gmail.com>
2018-04-18 14:39:49 +0100

    WIP: add share extension for ios

---
+++ ios/PeerioShare/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  PeerioShare
+//
+//  Created by work on 4/18/18.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
