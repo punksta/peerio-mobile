44962c43b5b2f5098f7db355b8e6967a255d9003
Alexandra Marin <alexandra.marin01@gmail.com>
2018-05-15 15:36:24 +0100

    WIP: add signup screens diff

---
+++ app/components/signup/signup-screens/medcryptor.js
@@ -0,0 +1,13 @@
+const SAVE_PIN_SCREEN_NUMBER = 2;
+const KEY_BACKED_SCREEN_NUMBER = 3;
+const PAGES = [
+    'signupStep1',
+    'signupMedical',
+    'signupAccountKey',
+    'signupConfirmBackup',
+    'signupContactSyncStart',
+    'signupContactAdd',
+    'signupContactInvite'
+];
+
+module.exports = { SAVE_PIN_SCREEN_NUMBER, KEY_BACKED_SCREEN_NUMBER, PAGES };
---
+++ app/components/signup/signup-screens/peerio.js
@@ -0,0 +1,13 @@
+const SAVE_PIN_SCREEN_NUMBER = 1;
+const KEY_BACKED_SCREEN_NUMBER = 2;
+const PAGES = [
+    'signupStep1',
+    'signupAccountKey',
+    'signupConfirmBackup',
+    'signupContactSyncStart',
+    'signupContactAdd',
+    'signupContactInvite'
+];
+
+module.exports = { SAVE_PIN_SCREEN_NUMBER, KEY_BACKED_SCREEN_NUMBER, PAGES };
+
