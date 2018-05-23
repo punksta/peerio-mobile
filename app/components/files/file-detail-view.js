998d6277cf6e5430f646d8483592c8c0c0bf2e8b
suprko <lucas@peerio.com>
2018-05-23 14:07:10 -0400

    WIP: fix file-detail-view

--- app/components/files/file-detail-view.js
+++ app/components/files/file-detail-view.js
@@ -1,205 +1,87 @@
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { action } from 'mobx';
-import moment from 'moment';
+import Text from '../controls/custom-text';
 import SafeComponent from '../shared/safe-component';
-import icons from '../helpers/icons';
 import { vars } from '../../styles/styles';
 import FileProgress from './file-progress';
-import FileActions from './file-actions';
 import { fileState } from '../states';
-import { T, tx } from '../utils/translator';
 import FileTypeIcon from './file-type-icon';
 import { fileHelpers } from '../../lib/icebear';
-import Text from '../controls/custom-text';
+import FileActionSheet from './file-action-sheet';
+import ButtonText from '../controls/button-text';
+import MenuIcon from '../layout/menu-icon';
+import { tx } from '../utils/translator';
+import snackbarState from '../snackbars/snackbar-state';
 
-const containerStyle = {
-    flexGrow: 1,
-    justifyContent: 'space-between',
-    marginHorizontal: vars.spacing.small.midi
+const textStyle = {
+    textAlign: 'center',
+    fontSize: vars.font.size.bigger,
+    color: vars.extraSubtleText,
+    marginBottom: vars.spacing.medium.mini2x
 };
 
-const firstRowStyle = {
-    flex: 0,
-    flexDirection: 'row',
-    marginTop: vars.spacing.small.maxi2x,
-    paddingRight: vars.spacing.medium.mini2x
+const fileProgressContainer = {
+    justifyContent: 'center',
+    height: vars.progressBarHeight,
+    paddingHorizontal: vars.spacing.huge.midi2x,
+    marginTop: vars.spacing.medium.midi2x,
+    marginBottom: vars.spacing.small.midi2x
 };
 
-const iconTypeStyle = {
-    marginLeft: vars.spacing.small.mini2x,
-    marginRight: vars.spacing.small.maxi2x
-};
-
-const secondRowStyle = {
-    marginTop: vars.spacing.medium.mini2x
-};
-
-const firstColumnStyle = {
-    flexGrow: 1,
-    flexShrink: 1,
-    paddingTop: vars.iconPadding
-};
-
-const hintStyle = {
-    color: 'rgba(0,0,0,.54)'
+const centered = {
+    alignItems: 'center'
 };
 
 @observer
 export default class FileDetailView extends SafeComponent {
-    get file() {
-        return fileState.currentFile || {};
+    get file() { return fileState.currentFile || {}; }
+
+    get rightIcon() { return (<MenuIcon action={() => FileActionSheet.show(this.file, false, 'files')} />); }
+
+    get enabled() {
+        return this.file && this.file.readyForDownload || fileState.showSelection;
     }
 
-    get actionsBar() {
-        return <FileActions file={this.file} />;
+    @action.bound onCancel() {
+        fileState.cancelDownload(this.file);
     }
 
-    @action.bound fileAction() {
+    @action.bound onOpen() {
         const { file } = this;
-        const enabled = file && file.readyForDownload || fileState.showSelection;
-        if (enabled) {
-            if (file && !file.isPartialDownload && file.cached) {
-                file.launchViewer();
-            } else {
-                fileState.download(file);
-            }
-        }
+        file.launchViewer().catch(() => {
+            snackbarState.pushTemporary(tx('snackbar_couldntOpenFile'));
+        });
     }
 
-    legacyNotification() {
-        const notificationContainer = {
-            marginTop: vars.spacing.medium.mini2x,
-            marginHorizontal: vars.spacing.small.maxi
-        };
-        const titleContainer = {
-            flexDirection: 'row',
-            backgroundColor: vars.legacyFileTitleBg,
-            borderRadius: 5,
-            marginBottom: vars.spacing.small.midi2x,
-            paddingVertical: vars.spacing.small.mini,
-            paddingHorizontal: vars.spacing.small.mini2x
-        };
-        const titleStyle = {
-            fontSize: vars.font.size.smallerx,
-            color: vars.legacyFileTitle,
-            paddingVertical: 1
-        };
-        const descriptionStyle = {
-            fontSize: vars.font.size.normal,
-            color: vars.subtleText
-        };
-        const learnMoreStyle = {
-            fontSize: vars.font.size.normal,
-            color: vars.peerioBlue,
-            marginTop: vars.spacing.medium.mini2x,
-            marginBottom: vars.spacing.small.midi
-        };
-        return (
-            <View style={notificationContainer}>
-                <View style={{ flexDirection: 'row' }}>
-                    <View style={titleContainer}>
-                        <Text style={titleStyle}>{tx('title_pending')}</Text>
-                    </View>
-                </View>
-                <View>
-                    <Text style={descriptionStyle}>{tx('title_oldVersionDescription1')}</Text>
-                    <Text style={descriptionStyle}>{tx('title_oldVersionDescription2')}</Text>
-                </View>
-                <Text semibold italic style={learnMoreStyle}><T k="title_learnMoreLegacyFiles" /></Text>
-            </View>);
-    }
-
-    renderFileDetailView() {
-        const { file } = this;
-        let icon = null;
-        if (file.downloading) icon = 'file-download';
-        if (file.uploading) icon = 'file-upload';
-        if (icon) icon = icons.plaindark(icon, vars.iconFileViewSize);
-        return (
-            <View style={containerStyle}>
-                <View>
-                    {file.isLegacy && this.legacyNotification()}
-                    <View style={firstRowStyle}>
-                        <View style={iconTypeStyle}>
-                            <TouchableOpacity
-                                onPress={this.fileAction}
-                                pressRetentionOffset={vars.pressRetentionOffset}>
-                                {icon ||
-                                <FileTypeIcon
-                                    size="large"
-                                    type={fileHelpers.getFileIconType(file.ext)}
-                                />}
-                            </TouchableOpacity>
-                        </View>
-                        <View style={firstColumnStyle}>
-                            <View style={{ flexGrow: 1, flexShrink: 1 }}>
-                                <TouchableOpacity
-                                    onPress={this.fileAction}
-                                    pressRetentionOffset={vars.pressRetentionOffset}>
-                                    <Text
-                                        ellipsizeMode="tail"
-                                        numberOfLines={1}>{file.name}</Text>
-                                </TouchableOpacity>
-                            </View>
-                            <View style={secondRowStyle}>
-                                <View style={{ flexGrow: 1 }}>
-                                    <Text style={hintStyle}>{tx('title_fileSize')}</Text>
-                                    <Text>{file.sizeFormatted} {file.ext}</Text>
-                                </View>
-
-                                <View style={{ flexGrow: 1 }}>
-                                    <Text style={hintStyle}>{tx('title_uploaded')}</Text>
-                                    <Text>{moment(file.uploadedAt).format(`MM/DD/YY HH:MM:SS a`)}</Text>
-                                </View>
-                            </View>
-                        </View>
-                    </View>
-                    <View style={secondRowStyle}>
-                        <FileProgress file={file} />
-                    </View>
-                </View>
-            </View>
-        );
-    }
-
-    renderFolderDetailView() {
-        const { file } = this;
-        const folder = file; // for the sake of readability
-        return (
-            <View style={containerStyle}>
-                <View style={firstRowStyle}>
-                    <View style={iconTypeStyle}>
-                        {icons.darkNoPadding('folder', null, null, vars.iconFileViewSize)}
-                    </View>
-                    <View style={firstColumnStyle}>
-                        <View style={{ flexGrow: 1, flexShrink: 1 }}>
-                            <Text
-                                ellipsizeMode="tail"
-                                numberOfLines={1}>{folder.name}
-                            </Text>
-                        </View>
-                        <View style={secondRowStyle}>
-                            <View style={{ flexGrow: 1 }}>
-                                <Text style={hintStyle}>{tx('title_folderSize')}</Text>
-                                <Text>{folder.sizeFormatted}</Text>
-                            </View>
-                            <View style={{ flexGrow: 1 }}>
-                                {folder.createdAt && <Text style={hintStyle}>{tx('title_created')}</Text>}
-                                {folder.createdAt && <Text>{moment(folder.createdAt).format(`MMM DD, YYYY`)}</Text>}
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </View>
-        );
+    @action.bound onDownload() {
+        fileState.download(this.file);
     }
 
     renderThrow() {
-        const { file } = this;
-        return (file.isFolder ?
-            this.renderFolderDetailView() :
-            this.renderFileDetailView());
+        const { file, enabled } = this;
+
+        let button;
+        if (file.downloading) button = <ButtonText text={tx('button_cancel')} onPress={this.onCancel} disabled={!enabled} />;
+        else if (file.hasFileAvailableForPreview) button = <ButtonText text={tx('button_open')} onPress={this.onOpen} disabled={!enabled} />;
+        else button = <ButtonText text={tx('button_download')} onPress={this.onDownload} disabled={!enabled} />;
+
+        return (
+            <View style={{ flexGrow: 1, justifyContent: 'center', backgroundColor: vars.darkBlueBackground05 }}>
+                <View style={centered}>
+                    {<FileTypeIcon size="medium" type={fileHelpers.getFileIconType(file.ext)} /> }
+                </View>
+                <View style={fileProgressContainer}>
+                    {file.downloading && <FileProgress file={file} />}
+                </View>
+                <View style={centered}>
+                    <Text style={textStyle}>
+                        {tx(file.downloading ? 'title_downloadingFile' : 'title_noPreview')}
+                    </Text>
+                    {button}
+                </View>
+            </View>
+        );
     }
 }
