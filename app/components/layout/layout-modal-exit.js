938985c9c2ea156ae0ff8ea6e30736bb4529c823
Samvel Avanesov <seavan@gmail.com>
2018-05-31 14:44:26 -0400

    feat: use commonheader in layoutmodalexit

--- app/components/layout/layout-modal-exit.js
+++ app/components/layout/layout-modal-exit.js
@@ -2,39 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react/native';
 import { View } from 'react-native';
-import Text from '../controls/custom-text';
 import SafeComponent from '../shared/safe-component';
 import Layout1 from '../layout/layout1';
+import ModalHeader from '../shared/modal-header';
 import icons from '../helpers/icons';
 import { vars } from '../../styles/styles';
 
 @observer
 export default class LayoutModalExit extends SafeComponent {
     exitRow() {
-        const container = {
-            flexGrow: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            padding: vars.spacing.small.mini2x,
-            paddingTop: vars.statusBarHeight * 2,
-            paddingBottom: 0,
-            backgroundColor: vars.darkBlueBackground15
-        };
-        const textStyle = {
-            flexGrow: 1,
-            flexShrink: 1,
-            flex: 1,
-            fontSize: vars.font.size.normal,
-            textAlign: 'center',
-            color: 'rgba(0, 0, 0, .54)'
-        };
-        return (
-            <View style={container}>
-                {icons.dark('close', () => this.props.onClose())}
-                <Text semibold style={textStyle} ellipsizeMode="tail" numberOfLines={1}>{this.props.title}</Text>
-                {this.props.rightIcon || icons.placeholder()}
-            </View>
-        );
+        const { title, rightIcon } = this.props;
+        const leftIcon = icons.dark('close', () => this.props.onClose());
+        const fontSize = vars.font.size.normal;
+        const outerStyle = { marginBottom: 0 };
+        return <ModalHeader {...{ leftIcon, rightIcon, title, fontSize, outerStyle }} />;
     }
 
     lineBlock(content) {
