8a03d9618028243800c072c315c463fe8168bb2a
Alexandra Marin <alexandra.marin01@gmail.com>
2018-08-02 15:35:49 +0300

    WIP: show alert

--- ios/PeerioShare/ShareViewController.swift
+++ ios/PeerioShare/ShareViewController.swift
@@ -33,50 +33,52 @@ class ShareViewController: UIViewController {
   }
   
   override func viewDidAppear(_ animated: Bool) {
-//    self.openURL(URL(string: "peerioshare://hello")!)
-    let group = DispatchGroup()
+    let alertView = UIAlertController(title: "Peerio", message: "Uploading file(s)", preferredStyle: .alert)
+    self.present(alertView, animated: true, completion: {
+      let group = DispatchGroup()
 
-    for item: Any in self.extensionContext!.inputItems {
-      let inputItem = item as! NSExtensionItem
-      for provider: Any in inputItem.attachments! {
-        let itemProvider = provider as! NSItemProvider
+      for item: Any in self.extensionContext!.inputItems {
+        let inputItem = item as! NSExtensionItem
+        for provider: Any in inputItem.attachments! {
+          let itemProvider = provider as! NSItemProvider
 
-        group.enter()
-        itemProvider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { data, error in
-          if error == nil {
-            let sharedItemUri = data as! URL
-            let pathToCopyTo = "\(self.sharedFolderPath)/\(sharedItemUri.pathComponents.last ?? "")"
-            try? FileManager.default.copyItem(at: sharedItemUri, to: URL(fileURLWithPath: pathToCopyTo))
-          } else {
-            NSLog("\(error)")
+          group.enter()
+          itemProvider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { data, error in
+            if error == nil {
+              let sharedItemUri = data as! URL
+              let pathToCopyTo = "\(self.sharedFolderPath)/\(sharedItemUri.pathComponents.last ?? "")"
+              try? FileManager.default.copyItem(at: sharedItemUri, to: URL(fileURLWithPath: pathToCopyTo))
+            } else {
+              NSLog("\(error)")
+            }
+            group.leave()
           }
-          group.leave()
         }
       }
-    }
-    
-    group.notify(queue: DispatchQueue.main) {
-      let files = try! FileManager.default.contentsOfDirectory(atPath: self.sharedFolderPath)
       
-      do {
-        let jsonData : Data = try JSONSerialization.data(
-          withJSONObject: [
-            "incoming-files" : files
-          ],
-          options: JSONSerialization.WritingOptions.init(rawValue: 0))
-        let jsonString = (NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)! as String).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        print("peerioshare://shared?\(jsonString!)")
-        let result = self.openURL(URL(string: "peerioshare://shared?\(jsonString!)")!)
-      } catch let error {
-        NSLog("\(error)")
+      group.notify(queue: DispatchQueue.main) {
+        let files = try! FileManager.default.contentsOfDirectory(atPath: self.sharedFolderPath)
+        
+        do {
+          let jsonData : Data = try JSONSerialization.data(
+            withJSONObject: [
+              "incoming-files" : files
+            ],
+            options: JSONSerialization.WritingOptions.init(rawValue: 0))
+          let jsonString = (NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)! as String).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+          print("peerioshare://shared?\(jsonString!)")
+          self.openURL(URL(string: "peerioshare://shared?\(jsonString!)")!)
+        } catch let error {
+          NSLog("\(error)")
+        }
+        
+        self.dismiss(animated: false) {
+          self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        }
       }
-      
-      self.dismiss(animated: false) {
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-      }
-    }
+    })
   }
-
+  
   @objc
   func openURL(_ url: URL) -> Bool {
     var responder: UIResponder? = self
