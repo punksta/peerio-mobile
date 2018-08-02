8693e83127661c55e52108728f70172753fdf629
Alexandra Marin <alexandra.marin01@gmail.com>
2018-08-02 13:30:10 +0300

    WIP: add shared group

--- ios/PeerioShare/ShareViewController.swift
+++ ios/PeerioShare/ShareViewController.swift
@@ -7,15 +7,62 @@
 
 import UIKit
 import Social
+import MobileCoreServices
 
 class ShareViewController: UIViewController {
-
+  var sharedFolderPath = ""
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let containerURL = FileManager().containerURL(forSecurityApplicationGroupIdentifier: "group.peerio.share")!
+    sharedFolderPath = "\(containerURL.path)/sharedData"
+    
+    do {
+      try FileManager.default.createDirectory(atPath: sharedFolderPath, withIntermediateDirectories: true, attributes: nil)
+    } catch let error as NSError {
+      print("Could not create the directory \(error)")
+    } catch {
+      fatalError()
+    }
+    
+    let files = try! FileManager.default.contentsOfDirectory(atPath: sharedFolderPath)
+    for file in files {
+      try? FileManager.default.removeItem(at: URL(fileURLWithPath: "\(sharedFolderPath)/\(file)"))
+    }
   }
   
   override func viewDidAppear(_ animated: Bool) {
-    self.openURL(URL(string: "peerioshare://hello")!)
+//    self.openURL(URL(string: "peerioshare://hello")!)
+    print(sharedFolderPath)
+    let group = DispatchGroup()
+
+    for item: Any in self.extensionContext!.inputItems {
+      let inputItem = item as! NSExtensionItem
+      for provider: Any in inputItem.attachments! {
+        let itemProvider = provider as! NSItemProvider
+
+        group.enter()
+        itemProvider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { data, error in
+//          if error == nil {
+              print(data, error)
+////            let url = data as! URL
+////            let path = "\(self.docPath)/\(url.pathComponents.last ?? "")"
+////            print(">>> sharepath: \(String(describing: url.path))")
+////
+////            try? FileManager.default.copyItem(at: url, to: URL(fileURLWithPath: path))
+//
+//          } else {
+//            NSLog("\(error)")
+//          }
+          group.leave()
+        }
+      }
+    }
+    
+    self.dismiss(animated: false) {
+      self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
   }
 
   @objc
