31dc26316caa067c441065b7bf6cb03efae40fad
Samvel Avanesov <seavan@gmail.com>
2018-08-21 01:15:01 -0400

    chore: update remove crypto require script

--- scripts/remove-require-crypto.sh
+++ scripts/remove-require-crypto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Removing require('crypto') references"
-perl -pi -e 's/require\(.crypto.\)/null/g' app/lib/peerio-icebear/crypto/util.random.js
+perl -pi -e 's/require\(.crypto.\)/null/g' app/lib/peerio-icebear/crypto/util/random.js
 perl -pi -e 's/require\(.crypto.\)/null/g' node_modules/sjcl/sjcl.js
 perl -pi -e 's/require\(.crypto.\)/null/g' node_modules/tweetnacl/nacl-fast.js
 echo "Removing require('minipdf') references"
