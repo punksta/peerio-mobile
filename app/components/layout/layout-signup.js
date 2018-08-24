188cdfaf1ee1786733dc325d367966ebbc6ee7e0
Samvel Avanesov <seavan@gmail.com>
2018-08-24 00:35:06 -0400

    chore: scroll support for layout signup

--- app/components/layout/layout-signup.js
+++ app/components/layout/layout-signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { View, StatusBar } from 'react-native';
+import { View, ScrollView, StatusBar } from 'react-native';
 import SafeComponent from '../shared/safe-component';
 import SnackBarConnection from '../snackbars/snackbar-connection';
 
@@ -22,16 +22,17 @@ export default class LayoutSignup extends SafeComponent {
 
     renderThrow() {
         return (
-            <View style={{
-                flex: 1
-            }}>
+            <ScrollView
+                automaticallyAdjustContentInsets={false}
+                alwaysBounceVertical={false}
+                style={{ flexGrow: 1 }}>
                 {this.props.body}
                 {this.props.footer}
                 <View style={{ position: 'absolute', bottom: 0, right: 0, left: 0 }}>
                     <SnackBarConnection />
                 </View>
                 <StatusBar hidden key="statusBar" />
-            </View>
+            </ScrollView>
         );
     }
 }
