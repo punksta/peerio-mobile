4f36a6e748caad4ea3ef4f6535b9b9ff7266c468
Samvel Avanesov <seavan@gmail.com>
2018-08-28 16:21:06 -0400

    chore: eslint

--- app/components/signup/signup-generate-ak.js
+++ app/components/signup/signup-generate-ak.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { action } from 'mobx';
 import { observer } from 'mobx-react/native';
 import { View } from 'react-native';
 import Text from '../controls/custom-text';
