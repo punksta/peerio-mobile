f4d80b024cd01883a0975cfcb6abae03b7305a49
Alexandra Marin <alexandra.marin01@gmail.com>
2018-09-06 20:57:37 +0300

    fix: use tx

--- app/components/shared/beacon-labels.js
+++ app/components/shared/beacon-labels.js
@@ -1,11 +1,13 @@
+import { tx } from '../utils/translator';
+
 const beaconLabels = {
     'mobile-chat-icon': {
-        textHeader: 'See all your conversations here.',
-        textLine1: 'Start a new conversation or check-in on your direct messages and rooms.'
+        textHeader: tx('See all your conversations here.'),
+        textLine1: tx('Start a new conversation or check-in on your direct messages and rooms.')
     },
     'mobile-contacts-icon': {
-        textHeader: 'See all your contacts here.',
-        textLine1: 'Invite people or see who already uses peerio in your contacts.'
+        textHeader: tx('See all your contacts here.'),
+        textLine1: tx('Invite people or see who already uses peerio in your contacts.')
     }
 };
 
